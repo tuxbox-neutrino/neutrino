2d39b0f036b954e752c065f2f6e97a0d0992e3a1
mws <mws@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2009-12-16 15:57:10 +0000

    comment unused var
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@68 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/gui/alphasetup.cpp
+++ src/gui/alphasetup.cpp
@@ -101,7 +101,7 @@ int CAlphaSetup::exec(CMenuTarget* parent, const std::string &)
 	paint();
 
 	int selected = 0;
-	int max = 1;
+//	int max = 1;
 
 	unsigned long long timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_MENU] == 0 ? 0xFFFF : g_settings.timing[SNeutrinoSettings
 ::TIMING_MENU]);
