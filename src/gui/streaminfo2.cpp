629c91063fbf978f9abe1f77d28d22f1848139a3
satbaby <satbaby@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2010-02-08 19:53:54 +0000

    show freq for cabel
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@306 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/gui/streaminfo2.cpp
+++ src/gui/streaminfo2.cpp
@@ -595,6 +595,11 @@ void CStreamInfo2::paint_techinfo(int xpos, int ypos)
 		g_Font[font_info]->RenderString(xpos, ypos, width*2/3-10, "Tp. Freq.:" , COL_MENUCONTENTDARK, 0, true); // UTF-8
 		g_Font[font_info]->RenderString(xpos+spaceoffset, ypos, width*2/3-10, buf, COL_MENUCONTENTDARK, 0, true); // UTF-8
 	}
+	else if(frontend->getInfo()->type == FE_QAM) {
+		sprintf((char*) buf, "%d.%d MHz", si.tsfrequency/1000000, si.tsfrequency%1000);
+		g_Font[font_info]->RenderString(xpos, ypos, width*2/3-10, "Tp. Freq.:" , COL_MENUCONTENTDARK, 0, true); // UTF-8
+		g_Font[font_info]->RenderString(xpos+spaceoffset, ypos, width*2/3-10, buf, COL_MENUCONTENTDARK, 0, true); // UTF-8
+	}
 	// paint labels
 	spaceoffset = g_Font[font_info]->getRenderWidth("VTXTpid:");
 
