df409ec254b5e61b3f6719a0552a66c0a3fc22f1
satbaby <satbaby@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2010-02-11 15:52:23 +0000

    cabel scaling
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@319 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/gui/streaminfo2.cpp
+++ src/gui/streaminfo2.cpp
@@ -607,6 +607,7 @@ void CStreamInfo2::paint_techinfo(int xpos, int ypos)
 		sprintf((char*) buf, "%d.%d MHz", si.tsfrequency/1000, si.tsfrequency%1000);
 		g_Font[font_info]->RenderString(xpos, ypos, width*2/3-10, "Frequenz:" , COL_MENUCONTENTDARK, 0, true); // UTF-8
 		g_Font[font_info]->RenderString(xpos+spaceoffset, ypos, width*2/3-10, buf, COL_MENUCONTENTDARK, 0, true); // UTF-8
+		scaling = 20000;
 	}
 	// paint labels
 	spaceoffset = g_Font[font_info]->getRenderWidth("VTXTpid:");
