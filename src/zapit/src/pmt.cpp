299c7dda96be34967d3281fef4a2353432259916
satbaby <satbaby@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2010-02-14 23:40:21 +0000

    change ecm pid to caid
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@341 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/zapit/src/pmt.cpp
+++ src/zapit/src/pmt.cpp
@@ -429,27 +429,27 @@ int parse_pmt(CZapitChannel * const channel)
 				if ( ia < pmtlen - 4 )
 					if(buffer[ia]==0x09 && buffer[ia+1]>0) {
 						switch(buffer[ia+2]) {
-							case 0x06: pmt_caids[ci0++][0] = (buffer[ia+4] & 0x1f) << 8 | buffer[ia+5];
+							case 0x06: pmt_caids[ci0++][0] = (buffer[ia+2] & 0x1f) << 8 | buffer[ia+3];
 								   break;
-							case 0x17: pmt_caids[ci1++][1] = (buffer[ia+4] & 0x1f) << 8 | buffer[ia+5];
+							case 0x17: pmt_caids[ci1++][1] = (buffer[ia+2] & 0x1f) << 8 | buffer[ia+3];
 								   break;
-							case 0x01: pmt_caids[ci2++][2] = (buffer[ia+4] & 0x1f) << 8 | buffer[ia+5];
+							case 0x01: pmt_caids[ci2++][2] = (buffer[ia+2] & 0x1f) << 8 | buffer[ia+3];
 								   break;
-							case 0x05: pmt_caids[ci3++][3] = (buffer[ia+4] & 0x1f) << 8 | buffer[ia+5];
+							case 0x05: pmt_caids[ci3++][3] = (buffer[ia+2] & 0x1f) << 8 | buffer[ia+3];
 								   break;
-							case 0x18: pmt_caids[ci4++][4] = (buffer[ia+4] & 0x1f) << 8 | buffer[ia+5];
+							case 0x18: pmt_caids[ci4++][4] = (buffer[ia+2] & 0x1f) << 8 | buffer[ia+3];
 								   break;
-							case 0x0B: pmt_caids[ci5++][5] = (buffer[ia+4] & 0x1f) << 8 | buffer[ia+5];
+							case 0x0B: pmt_caids[ci5++][5] = (buffer[ia+2] & 0x1f) << 8 | buffer[ia+3];
 								   break;
-							case 0x0D: pmt_caids[ci6++][6] = (buffer[ia+4] & 0x1f) << 8 | buffer[ia+5];
+							case 0x0D: pmt_caids[ci6++][6] = (buffer[ia+2] & 0x1f) << 8 | buffer[ia+3];
 								   break;
-							case 0x09: pmt_caids[ci7++][7] = (buffer[ia+4] & 0x1f) << 8 | buffer[ia+5];
+							case 0x09: pmt_caids[ci7++][7] = (buffer[ia+2] & 0x1f) << 8 | buffer[ia+3];
 								   break;
-							case 0x26: pmt_caids[ci8++][8] = (buffer[ia+4] & 0x1f) << 8 | buffer[ia+5];
+							case 0x26: pmt_caids[ci8++][8] = (buffer[ia+2] & 0x1f) << 8 | buffer[ia+3];
 								   break;
-							case 0x4a: pmt_caids[ci9++][9] = (buffer[ia+4] & 0x1f) << 8 | buffer[ia+5];
+							case 0x4a: pmt_caids[ci9++][9] = (buffer[ia+2] & 0x1f) << 8 | buffer[ia+3];
 								   break;
-							case 0x0E: pmt_caids[ci10++][10] = (buffer[ia+4] & 0x1f) << 8 | buffer[ia+5];
+							case 0x0E: pmt_caids[ci10++][10] = (buffer[ia+2] & 0x1f) << 8 | buffer[ia+3];
 								   break;
 						} //switch
 					} // if
