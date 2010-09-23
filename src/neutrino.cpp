baabbbaebd11b0a6d697d7e6f9b2156f29d9e044
satbaby <satbaby@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2010-09-23 14:14:29 +0000

    /mini ca mode default
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@795 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -825,7 +825,7 @@ int CNeutrinoApp::loadSetup(const char * fname)
 	g_settings.progressbar_color = configfile.getBool("progressbar_color", true );
 	g_settings.infobar_show  = configfile.getInt32("infobar_show", 1);
 	g_settings.infobar_show_channellogo   = configfile.getInt32("infobar_show_channellogo"  , 3 );
-	g_settings.casystem_display = configfile.getInt32("casystem_display", 0 );
+	g_settings.casystem_display = configfile.getInt32("casystem_display", 2 );//mini ca mode default
 	g_settings.scrambled_message = configfile.getBool("scrambled_message", true );                                                                                                  
 	g_settings.volume_pos = configfile.getInt32("volume_pos", 0 );                                                                                                                  
 	g_settings.menu_pos = configfile.getInt32("menu_pos", 0 );
