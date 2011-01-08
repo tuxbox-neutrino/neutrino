cde78096a0d8ab70f00034640e5b30a33880d723
thilo <thilo@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2011-01-08 14:32:09 +0000

    *neutrino record_setup: increased menue width, (user request) because of possible longer record directories
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@1003 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/gui/record_setup.cpp
+++ src/gui/record_setup.cpp
@@ -57,7 +57,7 @@ CRecordSetup::CRecordSetup()
 {
 	frameBuffer = CFrameBuffer::getInstance();
 
-	width = w_max (35, 10); //%
+	width = w_max (50, 10); //%
 	hheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU_TITLE]->getHeight();
 	mheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU]->getHeight();
 	height = hheight+13*mheight+ 10;
