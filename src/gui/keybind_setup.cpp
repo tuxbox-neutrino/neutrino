f90b5dc2b139593d532bb43a041133b3dc7df692
thilo <thilo@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2011-01-26 11:04:47 +0000

    *neutrino keybind_setup: menu width changed to 40%
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@1056 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/gui/keybind_setup.cpp
+++ src/gui/keybind_setup.cpp
@@ -58,7 +58,7 @@ CKeybindSetup::CKeybindSetup()
 	keySetupNotifier = new CKeySetupNotifier;
 	keySetupNotifier->changeNotify(NONEXISTANT_LOCALE, NULL);
 
-	width = w_max (30, 10);
+	width = w_max (40, 10);
 	hheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU_TITLE]->getHeight();
 	mheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU]->getHeight();
 	height 	= hheight+13*mheight+ 10;
