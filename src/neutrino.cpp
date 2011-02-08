f379bb5beeafdffe79573f3a121460c24549c3e6
lucgas <lucgas@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2011-02-08 11:34:18 +0000

    please Adjust Your Temperament!
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@1120 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -2302,7 +2302,7 @@ int CNeutrinoApp::run(int argc, char **argv)
 		saveSetup(NEUTRINO_SETTINGS_FILE);
 	}
 
-#ifdef IGNORE_COMMUNITY_EFFORTS_COMPLETELY
+#ifdef 
         system("mkdir /media/sda1 2> /dev/null");
         system("mount /media/sda1 2> /dev/null");
         system("mkdir /media/sdb1 2> /dev/null");
