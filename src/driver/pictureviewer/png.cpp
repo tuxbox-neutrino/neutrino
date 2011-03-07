daf195c66261fbfc20ac8b773716bfe89a75ea4f
seife <seife@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2011-03-07 09:06:10 +0000

    libpng 1.5 compile fixes
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@1270 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/driver/pictureviewer/png.cpp
+++ src/driver/pictureviewer/png.cpp
@@ -52,7 +52,11 @@ int fh_png_load(const char *name,unsigned char **buffer,int* /*xp*/,int* /*yp*/)
 		return(FH_ERROR_FORMAT);
 	}
 
-	if(setjmp(png_ptr->jmpbuf))
+#if (PNG_LIBPNG_VER < 10500)
+	if (setjmp(png_ptr->jmpbuf))
+#else
+	if (setjmp(png_jmpbuf(png_ptr)))
+#endif
 	{
 		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
 		fclose(fh);
@@ -145,7 +149,11 @@ int fh_png_getsize(const char *name,int *x,int *y, int /*wanted_width*/, int /*w
 		return(FH_ERROR_FORMAT);
 	}
 
-	if(setjmp(png_ptr->jmpbuf))
+#if (PNG_LIBPNG_VER < 10500)
+	if (setjmp(png_ptr->jmpbuf))
+#else
+	if (setjmp(png_jmpbuf(png_ptr)))
+#endif
 	{
 		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
 		fclose(fh);
