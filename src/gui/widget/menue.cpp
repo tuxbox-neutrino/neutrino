423d7478c95637f338d78ec07f0fe56fade21014
seife <seife@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2011-04-01 21:18:59 +0000

    neutrino: fix segfault due to insufficient size for PIN string
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@1372 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/gui/widget/menue.cpp
+++ src/gui/widget/menue.cpp
@@ -1566,7 +1566,7 @@ int CMenuSeparator::paint(bool selected, bool /*last*/)
 
 bool CPINProtection::check()
 {
-	char cPIN[4];
+	char cPIN[5];
 	neutrino_locale_t hint = NONEXISTANT_LOCALE;
 	do
 	{
