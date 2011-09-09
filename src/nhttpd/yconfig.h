26766b5cc629dedcb7db0348703c8e939feda063
yjogol <yjogol@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2011-09-09 06:17:58 +0000

    nhttpd push version to 3.4 (iOS API-Level < 3.4 = API0)
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-beta@1680 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/nhttpd/yconfig.h
+++ src/nhttpd/yconfig.h
@@ -25,7 +25,7 @@
 //-----------------------------------------------------------------------------
 // General central Definitions <configure!>
 //-----------------------------------------------------------------------------
-#define HTTPD_VERSION 		"3.3.3"				// Webserver version  (can be overloaded)
+#define HTTPD_VERSION 		"3.4.0"				// Webserver version  (can be overloaded)
 #define YHTTPD_VERSION 		"1.3.2"				// Webserver version  (Version of yhttpd-core!)
 #define IADDR_LOCAL 		"127.0.0.1"			// local IP
 #define HTTPD_NAME 			"yhttpd"			// Webserver name (can be overloaded)
