7368fad7bac16a515e91321b6ee80d1131120ca3
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2011-10-02 21:05:54 +0200

    neutrino: Tripledragon can only record one TS at once

--- src/driver/record.h
+++ src/driver/record.h
@@ -49,7 +49,12 @@
 
 #define REC_MAX_APIDS 10
 #define FILENAMEBUFFERSIZE 1024
+#if HAVE_TRIPLEDRAGON
+/* I'm not able to get it to work with more than 1 recording at a time :-( */
+#define RECORD_MAX_COUNT 1
+#else
 #define RECORD_MAX_COUNT 8
+#endif
 
 #define TSHIFT_MODE_OFF 0
 #define TSHIFT_MODE_TEMPORAER 1
