81335a3eddfdc3ff1611590450ef271b774a4a1c
focus <focus@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2011-11-25 08:55:33 +0000

    Set connect timeout to 10 seconds, search for updates takes too long in case host not answer
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-beta@1887 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/system/httptool.cpp
+++ src/system/httptool.cpp
@@ -91,6 +91,7 @@ printf("url is %s\n", URL.c_str());
 		curl_easy_setopt(curl, CURLOPT_USERAGENT, userAgent.c_str());
 		curl_easy_setopt(curl, CURLOPT_NOSIGNAL, (long)1);
 		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 1800);
+		curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 10);
 #ifdef DEBUG
 		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
 #endif
