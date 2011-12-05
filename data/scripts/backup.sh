1cfdbecfef36a7e99c29a595436160201132d790
satbaby <satbaby@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2011-12-05 20:39:24 +0000

    backup.sh: more flexibility with backup.list
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-beta@1938 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- data/scripts/backup.sh
+++ data/scripts/backup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 DATE=`date +%Y-%m-%d-%H:%M:%S`
+BACKUPLIST=/bin/backup.list
+
 echo Backup to $1/settings_$DATE.tar
 cd /
-tar cf $1/settings_$DATE.tar /var/tuxbox/config/
+
+if [ ! -e $BACKUPLIST ]; then
+	tar cf $1/settings_$DATE.tar /var/tuxbox/config/
+else
+	tar cf $1/settings_$DATE.tar -T $BACKUPLIST
+fi
