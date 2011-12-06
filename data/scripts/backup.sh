1922213fc4552351161b445ce8297842859f502f
satbaby <satbaby@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2011-12-06 13:33:26 +0000

    backup.sh: rename backup.list to tobackup.conf & move to /var/tuxbox/config/t (thx svenhoefer)
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-beta@1948 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- data/scripts/backup.sh
+++ data/scripts/backup.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
-DATE=`date +%Y-%m-%d-%H:%M:%S`
-BACKUPLIST=/bin/backup.list
+
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+USRF="/var/tuxbox/config/tobackup.conf"
+
+if [ -e "${USRF}" ]; then
+# read user-files from $USRF
+	while read i
+	do [ "${i:0:1}" = "#" ] || TOBACKUP="$TOBACKUP ${i%%#*}"
+	done < $USRF
+
+else
+	TOBACKUP="/var/tuxbox/config/"
+fi
+
+# check existence
+RES=""
+for i in $TOBACKUP
+	do [ -e "$i" ] && RES="$RES $i"
+	done
+
+TOBACKUP=$(echo $RES)
 
 echo Backup to $1/settings_$DATE.tar
-cd /
 
-if [ ! -e $BACKUPLIST ]; then
-	tar cf $1/settings_$DATE.tar /var/tuxbox/config/
-else
-	tar cf $1/settings_$DATE.tar -T $BACKUPLIST
-fi
+tar -cf $1/settings_$DATE.tar $TOBACKUP
