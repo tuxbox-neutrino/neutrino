9ed9b61867c7910c85708fd9613df27683d7c1bc
focus <focus@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2012-01-08 12:55:12 +0000

    fastscan: add missed copyright header
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-beta@2050 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/zapit/include/zapit/fastscan.h
+++ src/zapit/include/zapit/fastscan.h
@@ -1,3 +1,24 @@
+/*
+        Neutrino-GUI  -   DBoxII-Project
+
+        Copyright (C) 2011 CoolStream International Ltd
+
+        License: GPLv2
+
+        This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation;
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to the Free Software
+        Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #ifndef _FASTSCAN_H_
 #define _FASTSCAN_H_
 
