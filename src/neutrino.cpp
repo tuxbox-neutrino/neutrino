f7bf5932a9040c41230cef92e242c6ef4220645a
thilo <thilo@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2012-01-17 19:44:42 +0000

    *neutrino media player menu: call moviebrowser from media player object
    We had 2 ways to open the moviebrowser and one was with parental lock, the
    other without.
    btw: Is it really necessary to lock here?
    Moviebrowser got its own configurable parental lock.
    
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-beta@2064 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -2131,21 +2131,8 @@ void CNeutrinoApp::RealRun(CMenuWidget &mainMenu)
 				media->exec(NULL, "");
 			}
 			else if( msg == CRCInput::RC_video || msg == CRCInput::RC_play ) {
-				bool show = true;
-				if ((g_settings.parentallock_prompt == PARENTALLOCK_PROMPT_ONSIGNAL) || (g_settings.parentallock_prompt == PARENTALLOCK_PROMPT_CHANGETOLOCKED)) {
-                                        CZapProtection* zapProtection = new CZapProtection( g_settings.parentallock_pincode, 0x100 );
-                                        show = zapProtection->check();
-					delete zapProtection;
-				}
-				if(show) {
-					//StopSubtitles();
-					if( mode == mode_radio )
-						videoDecoder->StopPicture();
-					CMoviePlayerGui::getInstance().exec(NULL, "tsmoviebrowser");
-					if( mode == mode_radio )
-						videoDecoder->ShowPicture(DATADIR "/neutrino/icons/radiomode.jpg");
-					//StartSubtitles();
-				}
+				//open moviebrowser via media player menu object
+				CMediaPlayerMenu::getInstance()->exec(NULL,"movieplayer");
 			}
 			else if (CRCInput::isNumeric(msg) && g_RemoteControl->director_mode ) {
 				g_RemoteControl->setSubChannel(CRCInput::getNumericValue(msg));
