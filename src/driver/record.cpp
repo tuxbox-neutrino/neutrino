36a1030a7202b50e8b63382f79b74b0880b693fd
thilo <thilo@e54a6e83-5905-42d5-8d5c-058d10e6a962>
2012-02-15 18:21:49 +0000

    *neutrino record: use option 'yes' in message as default to stop recording
    git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-beta@2104 e54a6e83-5905-42d5-8d5c-058d10e6a962

--- src/driver/record.cpp
+++ src/driver/record.cpp
@@ -1180,7 +1180,7 @@ int CRecordManager::exec(CMenuTarget* parent, const std::string & actionKey )
 		snprintf(rec_msg1, sizeof(rec_msg1)-1, "%s", g_Locale->getText(LOCALE_RECORDINGMENU_MULTIMENU_ASK_STOP_ALL));
 		snprintf(rec_msg, sizeof(rec_msg)-1, rec_msg1, records);
 		if(ShowMsgUTF(LOCALE_SHUTDOWN_RECODING_QUERY, rec_msg,
-			CMessageBox::mbrNo, CMessageBox::mbYes | CMessageBox::mbNo, NULL, 450, 30, false) == CMessageBox::mbrYes)
+			CMessageBox::mbrYes, CMessageBox::mbYes | CMessageBox::mbNo, NULL, 450, 30, false) == CMessageBox::mbrYes)
 		{
 			snprintf(rec_msg1, sizeof(rec_msg1)-1, "%s", g_Locale->getText(LOCALE_RECORDINGMENU_MULTIMENU_INFO_STOP_ALL));
 // focus: i think no sense for 2 loops, because this code run in the same thread as neutrino,
