f7dcb00fafea102305429ac6dbf2428267b58034
[CST] Focus <focus.cst@gmail.com>
2012-04-02 17:40:22 +0400

    zapit/include/zapit/scan.h: add missed copyright header

--- src/zapit/include/zapit/scan.h
+++ src/zapit/include/zapit/scan.h
@@ -1,5 +1,22 @@
 /*
- *  $Id: scan.h,v 1.26 2003/03/14 07:31:50 obi Exp $
+        Neutrino-GUI  -   DBoxII-Project
+
+        Copyright (C) 2011 CoolStream International Ltd
+
+        License: GPLv2
+
+        This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to the Free Software
+        Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 #ifndef __scan_h__
