f631c7b8306564e047d1c9b7d3e29d75c59d3e27
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2012-11-07 09:48:27 +0100

    infoviewer: simplify and fix display of "HD" and "SD" icons

--- src/gui/infoviewer_bb.cpp
+++ src/gui/infoviewer_bb.cpp
@@ -497,6 +497,7 @@ void CInfoViewerBB::showIcon_Resolution()
 				icon_name = NEUTRINO_ICON_RESOLUTION_1920;
 				break;
 			case 1088:
+			case 1080:
 				icon_name = NEUTRINO_ICON_RESOLUTION_1080;
 				break;
 			case 1440:
@@ -539,28 +540,12 @@ void CInfoViewerBB::showIcon_Resolution()
 		}
 		if (g_settings.infobar_show_res == 1) {//show simple resolution icon on infobar
 			videoDecoder->getPictureInfo(xres, yres, framerate);
-			switch (yres) {
-			case 1920:
-			case 1440:
-			case 1280:
-			case 1088:
-			case 720:
+			if (yres > 576)
 				icon_name = NEUTRINO_ICON_RESOLUTION_HD;
-				break;
-			case 704:
-			case 576:
-			case 544:
-			case 528:
-			case 480:
-			case 382:
-			case 352:
-			case 288:
+			else if (yres > 0)
 				icon_name = NEUTRINO_ICON_RESOLUTION_SD;
-				break;
-			default:
+			else
 				icon_name = NEUTRINO_ICON_RESOLUTION_000;
-				break;
-			}
 		}
 	}
 	showBBIcons(CInfoViewerBB::ICON_RES, icon_name);
