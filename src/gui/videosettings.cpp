e30f93c776bc6cc07b90d0237e2b562aee056deb
[CST] Focus <focus.cst@gmail.com>
2012-12-17 12:46:09 +0400

    gui/videosettings.cpp: fix video mode switch ask, fix menu redraw after video mode switch

--- src/gui/videosettings.cpp
+++ src/gui/videosettings.cpp
@@ -315,9 +315,9 @@ void CVideoSettings::setupVideoSystem(bool do_ask)
 				g_settings.video_Mode = prev_video_mode;
 				videoDecoder->SetVideoSystem(g_settings.video_Mode);
 			}
+			else
+				prev_video_mode = g_settings.video_Mode;
 		}
-		else
-			prev_video_mode = g_settings.video_Mode;
 	}
 }
 
@@ -358,6 +358,7 @@ bool CVideoSettings::changeNotify(const neutrino_locale_t OptionName, void * /* 
 	else if (ARE_LOCALES_EQUAL(OptionName, LOCALE_VIDEOMENU_VIDEOMODE))
 	{
 		setupVideoSystem(true/*ask*/);
+		return true;
 	}
 #if 0
         else if (ARE_LOCALES_EQUAL(OptionName, LOCALE_VIDEOMENU_CONTRAST))
