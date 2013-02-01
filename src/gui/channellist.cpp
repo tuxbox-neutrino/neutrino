1e45607ae1af87d6d3abd3e3ac43461f7090354e
svenhoefer <svenhoefer@svenhoefer.com>
2013-02-01 23:26:33 +0100

    - channellist.cpp: change color for infozone

--- src/gui/channellist.cpp
+++ src/gui/channellist.cpp
@@ -572,7 +572,7 @@ int CChannelList::show()
 
 	if (g_settings.channellist_minitv)
 	{
-		frameBuffer->paintBoxRel(x+width,y+theight,infozone_width,pig_height+infozone_height,COL_MENUHEAD_PLUS_0, RADIUS_LARGE, CORNER_BOTTOM);
+		frameBuffer->paintBoxRel(x+width,y+theight,infozone_width,pig_height+infozone_height,COL_MENUCONTENT_PLUS_0, RADIUS_LARGE, CORNER_BOTTOM);
 	}
 
 	paintHead();
@@ -2099,7 +2099,7 @@ void CChannelList::paint_pig (int _x, int _y, int w, int h)
 void CChannelList::paint_events(int index)
 {
 	readEvents(chanlist[index]->channel_id);
-	frameBuffer->paintBoxRel(x+ width,y+ theight+pig_height, infozone_width, infozone_height,COL_MENUHEAD_PLUS_0, RADIUS_LARGE, CORNER_BOTTOM);
+	frameBuffer->paintBoxRel(x+ width,y+ theight+pig_height, infozone_width, infozone_height,COL_MENUCONTENT_PLUS_0, RADIUS_LARGE, CORNER_BOTTOM);
 
 	char text1[10];
 	CChannelEventList::iterator e;
