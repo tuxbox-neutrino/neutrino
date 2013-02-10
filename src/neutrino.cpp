c4f8bf17a96da94a3233e4a7b928149066b63088
svenhoefer <svenhoefer@svenhoefer.com>
2013-02-10 22:00:22 +0100

    - neutrino.cpp: remove double determination of g_info.delivery_system

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -1828,8 +1828,6 @@ TIMER_START();
 	cpuFreq->SetCpuFreq(g_settings.cpufreq * 1000 * 1000);
 
 	g_info.delivery_system = CFEManager::getInstance()->getLiveFE()->getInfo()->type == FE_QPSK ? DVB_S : DVB_C;
-
-	g_info.delivery_system = CFEManager::getInstance()->getLiveFE()->getInfo()->type == FE_QPSK ? DVB_S : DVB_C;
 #if HAVE_TRIPLEDRAGON
 	/* only SAT-hd1 before rev 8 has fan, rev 1 is TD (compat hack) */
 	g_info.has_fan = (cs_get_revision() > 1 && cs_get_revision() < 8 && g_info.delivery_system == DVB_S);
