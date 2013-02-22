94883e087706eb33953ba99c733e9d9119281d70
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2013-02-22 17:16:43 +0100

    channellist: initialize values to satisfy valgrind

--- src/gui/channellist.cpp
+++ src/gui/channellist.cpp
@@ -2177,6 +2177,7 @@ void CChannelList::paint_events(int index)
 
 		evt.description = g_Locale->getText(LOCALE_EPGLIST_NOEVENTS);
 		evt.eventID = 0;
+		evt.startTime = 0;
 		evtlist.push_back(evt);
 	}
 
@@ -2237,6 +2238,7 @@ void CChannelList::readEvents(const t_channel_id channel_id)
 		CChannelEvent evt;
 		evt.description = g_Locale->getText(LOCALE_EPGLIST_NOEVENTS);
 		evt.eventID = 0;
+		evt.startTime = 0;
 		evtlist.push_back(evt);
 	}
 	else
