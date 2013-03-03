9064d34a1ec09ab65fd22e2241265169503242e5
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2013-03-03 15:16:35 +0100

    scanpmt: de-spam CPmt class

--- src/zapit/src/scanpmt.cpp
+++ src/zapit/src/scanpmt.cpp
@@ -93,7 +93,7 @@ bool CPmt::Parse(CZapitChannel * const channel)
 
 	ProgramMapSection pmt(buffer);
 
-	printf("[pmt] pcr pid: old 0x%x new 0x%x\n", channel->getPcrPid(), pmt.getPcrPid());
+	DBG("pcr pid: old 0x%x new 0x%x\n", channel->getPcrPid(), pmt.getPcrPid());
 
 	channel->resetPids();
 
@@ -182,14 +182,14 @@ bool CPmt::ParseEsInfo(ElementaryStreamInfo *esinfo, CZapitChannel * const chann
 				TeletextDescriptor *td = (TeletextDescriptor*)d;
 				const VbiTeletextList *vbilist = td->getVbiTeletexts();
 				VbiTeletextConstIterator it;
-				printf("[pmt] teletext pid %04x\n", esinfo->getPid());
+				DBG("teletext pid %04x\n", esinfo->getPid());
 				for (it = vbilist->begin(); it != vbilist->end(); ++it) {
 					VbiTeletext * vbi = *it;
 
 					std::string lang = vbi->getIso639LanguageCode();
 					uint8_t page = vbi->getTeletextPageNumber();
 					uint8_t magazine = vbi->getTeletextMagazineNumber();
-					printf("[pmt] teletext type %d mag %d page %d lang [%s]\n",
+					DBG("teletext type %d mag %d page %d lang [%s]\n",
 							vbi->getTeletextType(), magazine, page, lang.c_str());
 					if (vbi->getTeletextType() == 0x01)
 						channel->setTeletextLang(lang);
@@ -265,7 +265,7 @@ bool CPmt::ParseEsInfo(ElementaryStreamInfo *esinfo, CZapitChannel * const chann
 	case 0x1b: // AVC Video Stream (MPEG4 H264)
 		channel->setVideoPid(esinfo->getPid());
 		channel->type = (stream_type == 0x1b); //FIXME
-		printf("[pmt] vpid %04x stream %d type %d\n", esinfo->getPid(), stream_type, channel->type);
+		DBG("vpid %04x stream %d type %d\n", esinfo->getPid(), stream_type, channel->type);
 		break;
 	case 0x03:
 	case 0x04:
@@ -298,7 +298,7 @@ bool CPmt::ParseEsInfo(ElementaryStreamInfo *esinfo, CZapitChannel * const chann
 			snprintf(str, DESC_MAX_LEN, "Unknown");
 			description = str;
 		}
-		printf("[pmt] apid %04x stream %02x type %d [%s]\n", esinfo->getPid(), stream_type,
+		DBG("apid %04x stream %02x type %d [%s]\n", esinfo->getPid(), stream_type,
 				(int) audio_type, description.c_str());
 		if(CServiceScan::getInstance()->Scanning()) {
 			if(channel->getPreAudioPid() == 0)
@@ -394,7 +394,7 @@ int pmt_set_update_filter(CZapitChannel * const channel, int * fd)
 
 int pmt_stop_update_filter(int * fd)
 {
-	printf("[pmt] stop update filter\n");
+	DBG("\n");
 #if HAVE_TRIPLEDRAGON
 	if (pmtDemux)
 		delete pmtDemux;
