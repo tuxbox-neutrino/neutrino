e08ee68cd66374543a0e1bcc9ce57230eda8b2de
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2013-03-10 00:01:04 +0100

    bouquetlist: fix width calculation

--- src/gui/bouquetlist.cpp
+++ src/gui/bouquetlist.cpp
@@ -328,7 +328,7 @@ int CBouquetList::show(bool bShowChannelList)
 
 	for(unsigned int count = 0; count < sizeof(CBouquetListButtons)/sizeof(CBouquetListButtons[0]);count++){
 		int w_text = g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_SMALL]->getRenderWidth(g_Locale->getText (CBouquetListButtons[count].locale),true);
-		w_max_text = std::max(w_max_icon, w_text);
+		w_max_text = std::max(w_max_text, w_text);
 		frameBuffer->getIconSize(CBouquetListButtons[count].button, &icol_w, &icol_h);
 		w_max_icon = std::max(w_max_icon, icol_w);
 	}
