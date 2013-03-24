77ecbec4097bc26a6d9f93ff27357953024a3f47
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2013-03-24 13:11:07 +0100

    glthread: don't kill neutrino during shutdown

--- src/driver/glthread.cpp
+++ src/driver/glthread.cpp
@@ -201,10 +201,6 @@ void GLThreadObj::run()
 	{
 		g_RCInput->postMsg(NeutrinoMessages::SHUTDOWN, 0);
 	}
-	else
-	{ /* yeah, whatever... */
-		::kill(getpid(), SIGKILL);
-	}
 	std::cout << "GL thread stopping" << std::endl;
 }
 
