4e7f81a1261f734cb576bebb7e0d5920bda01a27
Thilo Graf <dbt@novatux.de>
2013-04-04 09:47:48 +0200

    CComponentsInfoBox: prevent background paint in infobox
    Background for text is not needed here

--- src/gui/components/cc_item_infobox.cpp
+++ src/gui/components/cc_item_infobox.cpp
@@ -127,6 +127,7 @@ void CComponentsInfoBox::paint(bool do_save_bg)
 		
 		cctext = new CComponentsText();
 		cctext->setText(ct_text, ct_text_mode, ct_font);
+		cctext->doPaintTextBoxBg(false);
 		cctext->setDimensionsAll(x_text, y+fr_thickness, width-(x_text-x+x_offset+fr_thickness), height-2*fr_thickness);
  		cctext->paint(CC_SAVE_SCREEN_NO);
 	}
