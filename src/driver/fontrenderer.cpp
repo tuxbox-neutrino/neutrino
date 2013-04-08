f3dcf52ea009266e34bf917ae633ef56a715c5ad
Jacek Jendrzej <crashdvb@googlemail.com>
2013-04-08 18:03:33 +0200

    revert 89251f648ea431eb2808e3e4e575b3dc1c25a1c8 && 89251f648ea431eb2808e3e4e575b3dc1c25a1c8

--- src/driver/fontrenderer.cpp
+++ src/driver/fontrenderer.cpp
@@ -432,14 +432,8 @@ void Font::RenderString(int x, int y, const int width, const char *text, const u
 	static fb_pixel_t oldbgcolor = 0, oldfgcolor = 0;
 	static fb_pixel_t colors[256]={0};
 
-	/* fetch bgcolor from framebuffer, using lower left edge of the font... */
-	fb_pixel_t bgcolor = *(frameBuffer->getFrameBufferPointer() + x +
-			       y * frameBuffer->getStride() / sizeof(fb_pixel_t));
-	// fb_pixel_t bgcolor = frameBuffer->realcolor[color];
-	uint8_t fgindex = color;		/* index of font color in the palette */
-	if (color > COL_BLACK0 && color < 254)	/* bigger than 254 would result in  > 255 */
-		fgindex = ((((int)color) + 2) | 7) - 2; /* no idea what this does exactly... */
-	fb_pixel_t fgcolor = frameBuffer->realcolor[fgindex];
+	fb_pixel_t bgcolor = frameBuffer->realcolor[color];
+	fb_pixel_t fgcolor = frameBuffer->realcolor[(((((int)color) + 2) | 7) - 2)];
 
 	if((oldbgcolor != bgcolor) || (oldfgcolor != fgcolor)) {
 
