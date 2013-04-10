838cf61a692095f4688251eace39bf5e32dbb865
Jacek Jendrzej <crashdvb@googlemail.com>
2013-04-10 20:43:32 +0200

    src/gui/channellist.cpp: fix to 4a8350c3824c2c337045a4b6d89e15543e3b2565

--- src/gui/channellist.cpp
+++ src/gui/channellist.cpp
@@ -1598,7 +1598,7 @@ void CChannelList::paintDetails(int index)
 		else
 			desc = desc + " (" + CServiceManager::getInstance()->GetSatelliteName(chanlist[index]->getSatellitePosition()) + ")";
 
-		g_Font[SNeutrinoSettings::FONT_TYPE_CHANNELLIST]->RenderString(x+ 10, y+ height+ 5+ 2*fheight, full_width - 30, desc.c_str(), COL_MENUCONTENTDARK, 0, true);
+		g_Font[SNeutrinoSettings::FONT_TYPE_CHANNELLIST]->RenderString(x+ 10, y+ height+ 5+ 2*fheight +fdescrheight, full_width - 30, desc.c_str(), COL_MENUCONTENTDARK, 0, true);
 	}
 	else if( !displayNext && g_settings.channellist_foot == 1) { // next Event
 		char buf[128] = {0};
