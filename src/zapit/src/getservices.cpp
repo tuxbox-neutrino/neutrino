b577322a79fb55bd8452fb79b2a6e2c37892639d
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2013-04-13 22:53:38 +0200

    getservices: add option to not save removed channels

--- src/zapit/src/getservices.cpp
+++ src/zapit/src/getservices.cpp
@@ -785,7 +785,7 @@ void CServiceManager::WriteSatHeader(FILE * fd, sat_config_t &config)
 	}
 }
 
-void CServiceManager::SaveServices(bool tocopy, bool if_changed)
+void CServiceManager::SaveServices(bool tocopy, bool if_changed, bool no_deleted)
 {
 	int processed = 0;
 
@@ -830,7 +830,9 @@ void CServiceManager::SaveServices(bool tocopy, bool if_changed)
 						tpdone = 1;
 					}
 
-					ccI->second.dumpServiceXml(fd);
+					/* don't dump removed channels if no_deleted == true */
+					if (!no_deleted || !(ccI->second.flags & CZapitChannel::REMOVED))
+						ccI->second.dumpServiceXml(fd);
 					processed++;
 #ifdef SAVE_DEBUG
 					chans_processed.insert(ccI->second.getChannelID());
