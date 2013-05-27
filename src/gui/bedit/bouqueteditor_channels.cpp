920e454274d4cabbf7da91d767516fa856de923a
svenhoefer <svenhoefer@svenhoefer.com>
2013-05-28 01:32:23 +0200

    - bouqueteditor: using CComponentsHeader to paint header
    * move menu button to header

--- src/gui/bedit/bouqueteditor_channels.cpp
+++ src/gui/bedit/bouqueteditor_channels.cpp
@@ -44,6 +44,7 @@
 #include <driver/fontrenderer.h>
 #include <driver/screen_max.h>
 #include "bouqueteditor_chanselect.h"
+#include <gui/components/cc_frm.h>
 #include <gui/widget/buttons.h>
 #include <gui/widget/icons.h>
 
@@ -167,8 +168,8 @@ void CBEChannelWidget::paint()
 
 void CBEChannelWidget::paintHead()
 {
-	frameBuffer->paintBoxRel(x,y, width,theight+0, COL_MENUHEAD_PLUS_0, RADIUS_LARGE, CORNER_TOP);
-	g_Font[SNeutrinoSettings::FONT_TYPE_MENU_TITLE]->RenderString(x+10,y+theight+0, width, caption.c_str() , COL_MENUHEAD, 0, true);
+	CComponentsHeader header(x, y, width, theight, caption.c_str(), NULL /*no header icon*/);
+	header.paint();
 }
 
 const struct button_label CBEChannelWidgetButtons[4] =
