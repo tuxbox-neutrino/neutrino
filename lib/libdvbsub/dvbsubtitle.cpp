31d0b88ca15e82cef2e34e491d8c5a0c93d2b669
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2013-06-01 17:17:28 +0200

    dvbsubtitle: add missing blit()

--- lib/libdvbsub/dvbsubtitle.cpp
+++ lib/libdvbsub/dvbsubtitle.cpp
@@ -180,10 +180,10 @@ void cDvbSubtitleBitmaps::Draw(int &min_x, int &min_y, int &max_x, int &max_y)
 		if(max_y < (yoff + nh))
 			max_y = yoff + nh;
 	}
-#ifdef HAVE_SPARK_HARDWARE
+
 	if (Count())	/* sync framebuffer */
 		CFrameBuffer::getInstance()->blit();
-#endif
+
 //	if(Count())
 //		dbgconverter("cDvbSubtitleBitmaps::Draw: finish, min/max screen: x=% d y= %d, w= %d, h= %d\n", min_x, min_y, max_x-min_x, max_y-min_y);
 //	dbgconverter("\n");
@@ -269,6 +269,7 @@ void cDvbSubtitleConverter::Clear(void)
 //	dbgconverter("cDvbSubtitleConverter::Clear: x=% d y= %d, w= %d, h= %d\n", min_x, min_y, max_x-min_x, max_y-min_y);
 	if(running && (max_x-min_x > 0) && (max_y-min_y > 0)) {
 		CFrameBuffer::getInstance()->paintBackgroundBoxRel (min_x, min_y, max_x-min_x, max_y-min_y);
+		CFrameBuffer::getInstance()->blit();
 		/* reset area to clear */
 		min_x = screen_w;
 		min_y = screen_h;
