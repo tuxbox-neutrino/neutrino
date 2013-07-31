b2141a8217099832738950c573c03fca776fb9d6
[CST] Focus <focus.cst@gmail.com>
2013-07-31 15:06:48 +0400

    autorun.sh: add reboot
    Conflicts:
    	autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -3,6 +3,17 @@ export PATH=${PATH}:/opt/bin:/opt/usr/bin
 
 echo "### Starting NEUTRINO ###"
 
-cd /opt/bin
-./neutrino &
+cd /tmp
+/bin/neutrino > /dev/null 2> /dev/null
 
+/bin/sync
+/bin/sync
+
+if [ -e /tmp/.reboot ] ; then
+    /bin/dt -t"Rebooting..."
+    /sbin/reboot -f
+else
+    /bin/dt -t"Panic..."
+    sleep 5
+    /sbin/reboot -f
+fi
