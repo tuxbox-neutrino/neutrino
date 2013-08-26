aca06a5c02278e8a8a26b3def0967e7141713514
Thilo Graf <dbt@novatux.de>
2013-08-26 21:41:04 +0200

    CComponentsShapeCircle: getDiam() become const
    Technically the member function 'CComponentsShapeCircle::getDiam'
    can be const (style)

--- src/gui/components/cc_item_shapes.h
+++ src/gui/components/cc_item_shapes.h
@@ -1,5 +1,5 @@
 /*
-	Based up Neutrino-GUI - Tuxbox-Project 
+	Based up Neutrino-GUI - Tuxbox-Project
 	Copyright (C) 2001 by Steffen Hehn 'McClean'
 
 	Classes for generic GUI-related components.
@@ -49,7 +49,7 @@ class CComponentsShapeCircle : public CComponentsItem
 		///set property: diam
 		inline void setDiam(const int& diam){d=width=height=diam, corner_rad=d/2;};
 		///get property: diam
-		inline int getDiam(){return d;};
+		inline int getDiam() const {return d;};
 
 		///paint item
 		void paint(bool do_save_bg = CC_SAVE_SCREEN_YES);
@@ -60,7 +60,7 @@ class CComponentsShapeSquare : public CComponentsItem
 	public:
 		CComponentsShapeSquare(	const int x_pos, const int y_pos, const int w, const int h, bool has_shadow = CC_SHADOW_ON,
 					fb_pixel_t color_frame = COL_MENUCONTENT_PLUS_6, fb_pixel_t color_body = COL_MENUCONTENT_PLUS_0, fb_pixel_t color_shadow = COL_MENUCONTENTDARK_PLUS_0);
-		
+
 		void paint(bool do_save_bg = CC_SAVE_SCREEN_YES);
 };
 
