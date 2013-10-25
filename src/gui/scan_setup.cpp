7bf76aad3dc6c87dd26e8e7d13330f9ded2162a5
[CST] Focus <focus.cst@gmail.com>
2013-10-25 16:17:55 +0400

    gui/scan_setup.cpp: fix broken zapit config for frontend setup

--- src/gui/scan_setup.cpp
+++ src/gui/scan_setup.cpp
@@ -524,6 +524,7 @@ int CScanSetup::showScanMenuFrontendSetup()
 
 	fe_restart = false;
 
+	CZapit::getInstance()->GetConfig(zapitCfg);
 	CMenuWidget * setupMenu = new CMenuWidget(LOCALE_SATSETUP_FE_SETUP, NEUTRINO_ICON_SETTINGS, width, MN_WIDGET_ID_SCAN_FE_SETUP);
 	setupMenu->addIntroItems();
 
