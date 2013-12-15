7e2d12e5d54911fd074f38d8a7b7683291e0c4b6
Jacek Jendrzej <crashdvb@googlemail.com>
2013-12-15 15:43:10 +0100

    CChannelList::~CChannelList(): fix possible segfault
    Signed-off-by: Jacek Jendrzej <crashdvb@googlemail.com>

--- src/gui/channellist.cpp
+++ src/gui/channellist.cpp
@@ -126,20 +126,26 @@ CChannelList::~CChannelList()
 {
 //printf("************ DELETE LIST %s : %x\n", name.c_str(), this);fflush(stdout);
 	chanlist.clear();
-	delete dline;
-	if (cc_minitv)
+	if(dline){
+		delete dline;
+		dline = NULL;
+	}
+	if (cc_minitv){
 		delete 	cc_minitv;
+		cc_minitv = NULL;
+	}
 	if (headerClock) {
 		headerClock->Stop();
 		if (headerClock->isPainted())
 			headerClock->hide();
 		if (headerClock->isClockRun())
 			headerClock->stopThread();
-		delete headerClock;
-		headerClock = NULL;
+		if(headerClock){
+			delete headerClock;
+			headerClock = NULL;
+		}		  
 	}
 }
-
 void CChannelList::ClearList(void)
 {
 //printf("************ CLEAR LIST %s : %x\n", name.c_str(), this);fflush(stdout);
