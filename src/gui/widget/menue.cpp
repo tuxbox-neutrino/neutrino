5cc2620008f42e0c2e2c4b1f15e1f3ec858dd7f0
[CST] Focus <focus.cst@gmail.com>
2013-12-15 14:56:12 +0400

    gui/widget/menue.cpp: revert testing changes

--- src/gui/widget/menue.cpp
+++ src/gui/widget/menue.cpp
@@ -785,8 +785,8 @@ void CMenuWidget::checkHints()
 			break;
 		}
 	}
-	//if (has_hints)
-	//	GenericMenuBack->setHint(NEUTRINO_ICON_HINT_BACK, LOCALE_MENU_HINT_BACK);
+	if (has_hints)
+		GenericMenuBack->setHint(NEUTRINO_ICON_HINT_BACK, LOCALE_MENU_HINT_BACK);
 }
 
 void CMenuWidget::calcSize()
