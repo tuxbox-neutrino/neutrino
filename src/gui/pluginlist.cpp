b427c3d6a559c61285728b220982f1b288d6a9dd
svenhoefer <svenhoefer@svenhoefer.com>
2014-02-08 22:43:03 +0100

    - pluginlist: don't show full list when pluginlist-size is smaller

--- src/gui/pluginlist.cpp
+++ src/gui/pluginlist.cpp
@@ -324,7 +324,10 @@ void CPluginList::paint()
 	width = w_max( 500, 0 );
 	height = h_max( 526, 50 ); // 2*25 pixel frei
 	listmaxshow = (height-theight-0)/fheight;
-	height = theight+0+listmaxshow*fheight; // recalc height
+	int multiplicator = pluginlist.size();
+	if (listmaxshow < pluginlist.size())
+		multiplicator = listmaxshow;
+	height = theight+0+multiplicator*fheight; // recalc height
 	x=getScreenStartX( width );
 	y=getScreenStartY( height );
 
