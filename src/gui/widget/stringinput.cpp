5d4286285a6f15c2ff453d0e9a4fa7b9c7c4f7a6
M. Liebmann <tuxcode.bbg@gmail.com>
2014-03-18 19:01:20 +0100

    CStringInputSMS: Add '%' to valid chars

--- src/gui/widget/stringinput.cpp
+++ src/gui/widget/stringinput.cpp
@@ -636,7 +636,7 @@ void CStringInputSMS::initSMS(const char * const Valid_Chars)
 {
 	last_digit = -1;				// no key pressed yet
 	const char CharList[10][11] = { "0 -_/()<>=",	// 10 characters
-					"1+.,:!?\\'",//' for c't search ;)
+					"1+.,:!?%\\'",  //' for c't search ;)
 					"abc2ä",
 					"def3",
 					"ghi4",
