20180b5b83da654bcb025b40fe2bcb37e9bc8a73
Thilo Graf <dbt@novatux.de>
2014-10-26 23:49:27 +0100

    CMenuWidget: fix applying of hintbox text color after changed theme

--- src/gui/widget/menue.cpp
+++ src/gui/widget/menue.cpp
@@ -1357,7 +1357,7 @@ void CMenuWidget::paintHint(int pos)
 		info_box->setDimensionsAll(x, ypos2, iwidth, hint_height);
 		info_box->setFrameThickness(2);
 		info_box->removeLineBreaks(str);
-		info_box->setText(str, CTextBox::AUTO_WIDTH, g_Font[SNeutrinoSettings::FONT_TYPE_MENU_HINT]);
+		info_box->setText(str, CTextBox::AUTO_WIDTH, g_Font[SNeutrinoSettings::FONT_TYPE_MENU_HINT], COL_MENUCONTENT_TEXT);
 		info_box->setCorner(RADIUS_LARGE);
 		info_box->syncSysColors();
 		info_box->setColorBody(COL_MENUCONTENTDARK_PLUS_0);
