e0f8cddeedadeae3dfeea3574acc90a7c7aa512c
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2015-02-06 19:59:43 +0100

    start_wizard: ensure sytem timezone settings are sane

--- src/gui/start_wizard.cpp
+++ src/gui/start_wizard.cpp
@@ -91,6 +91,10 @@ int CStartUpWizard::exec(CMenuTarget* parent, const string & /*actionKey*/)
 	languageSettings.showLanguageSetup(&osdl_setup);
 	osdl_setup.exec(NULL, "");
 
+	/* hack to ensure system's view of timezone is the same as neutrino's */
+	CTZChangeNotifier tzn;
+	tzn.changeNotify(NONEXISTANT_LOCALE, (void *)"dummy");
+
 #if 0
 	if(ShowMsg (LOCALE_WIZARD_WELCOME_HEAD, g_Locale->getText(LOCALE_WIZARD_WELCOME_TEXT), CMessageBox::mbrYes, CMessageBox::mbYes | CMessageBox::mbrCancel) == CMessageBox::mbrYes)
 #endif
