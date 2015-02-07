67bd1738a68d84ab403c07fb2ed85823ad18ad7f
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2015-02-08 00:55:44 +0100

    hdd_menu: my_popen() needs waitpid to avoid zombies

--- src/gui/hdd_menu.cpp
+++ src/gui/hdd_menu.cpp
@@ -37,6 +37,7 @@
 #include <sys/stat.h>
 #include <sys/swap.h>
 #include <sys/vfs.h>
+#include <sys/wait.h>
 #include <dirent.h>
 #include <dlfcn.h>
 #include <sys/mount.h>
@@ -197,6 +198,7 @@ void CHDDMenuHandler::getBlkIds()
 		hdd_list.push_back(hdd);
 	}
 	fclose(f);
+	waitpid(pid, NULL, 0); /* beware of the zombie apocalypse! */
 }
 
 std::string CHDDMenuHandler::getDefaultPart(std::string dev)
