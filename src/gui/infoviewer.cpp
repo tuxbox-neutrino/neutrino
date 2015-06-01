673bf4ea75ecc5e245829f5d54f314df9e56ab13
[CST] Focus <focus.cst@gmail.com>
2015-06-01 14:19:07 +0300

    gui/infoviewer.cpp: remove commented setServiceChanged call

--- src/gui/infoviewer.cpp
+++ src/gui/infoviewer.cpp
@@ -839,12 +839,6 @@ void CInfoViewer::showTitle (const int ChanNum, const std::string & Channel, con
 	showLcdPercentOver ();
 	showInfoFile();
 
-#if 0
-	if ((g_RemoteControl->current_channel_id == channel_id) && !(((info_CurrentNext.flags & CSectionsdClient::epgflags::has_next) && (info_CurrentNext.flags & (CSectionsdClient::epgflags::has_current | CSectionsdClient::epgflags::has_no_current))) || (info_CurrentNext.flags & CSectionsdClient::epgflags::not_broadcast))) {
-		g_Sectionsd->setServiceChanged (channel_id, true);
-	}
-#endif
-
 	// Radiotext
 	if (CNeutrinoApp::getInstance()->getMode() == NeutrinoMessages::mode_radio)
 	{
