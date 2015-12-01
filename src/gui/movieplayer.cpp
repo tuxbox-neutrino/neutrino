68221e31b818a69194ce5e6074601a2d9ae41c8f
svenhoefer <svenhoefer@svenhoefer.com>
2015-12-01 22:40:05 +0100

    Revert "- movieplayer: enable videoformat and 4:3 switches; (C) martii"
    This reverts commit a3ff096e06bec9bab88b722f87737e50a937bcaa,
    because keys was passed to neutrino handleMsg already.
    So this pick was unnecessary.

--- src/gui/movieplayer.cpp
+++ src/gui/movieplayer.cpp
@@ -983,10 +983,6 @@ void CMoviePlayerGui::PlayFileLoop(void)
 				repeat_mode = REPEAT_OFF;
 			g_settings.movieplayer_repeat_on = repeat_mode;
 			callInfoViewer();
-		} else if( msg == (neutrino_msg_t) g_settings.key_next43mode) {
-			g_videoSettings->next43Mode();
-		} else if( msg == (neutrino_msg_t) g_settings.key_switchformat) {
-			g_videoSettings->SwitchFormat();
 		} else if (msg == (neutrino_msg_t) CRCInput::RC_setup) {
 			CNeutrinoApp::getInstance()->handleMsg(NeutrinoMessages::SHOW_MAINMENU, 0);
 		} else if (msg == (neutrino_msg_t) g_settings.mpkey_play) {
