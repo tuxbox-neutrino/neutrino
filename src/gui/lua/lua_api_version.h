96d7401e082c6af5d9e8ffc1deada3fd0be1017d
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2016-01-01 14:14:55 +0100

    edit lua_api_version.h instead of autogenerating it
    This will help those who do not use ccache :-)

---
+++ src/gui/lua/lua_api_version.h
@@ -0,0 +1,7 @@
+/*
+ * this is only used in two code files, so put it into its own
+ * header to reduce complete neutrino rebuilding all the time due
+ * to luainstance.h changes
+ */
+#define LUA_API_VERSION_MAJOR 1
+#define LUA_API_VERSION_MINOR 37
