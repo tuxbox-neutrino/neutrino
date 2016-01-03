b36c2b03d6d69f2610637cf9a162acba733df89b
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2016-01-03 18:41:52 +0100

    set_threadname: silence "overlapping src/dest" valgrind warning
    This might be a false positive, but the fix does not harm ;)

--- src/system/set_threadname.h
+++ src/system/set_threadname.h
@@ -7,7 +7,7 @@
 inline void set_threadname(const char *name)
 {
 	char threadname[17];
-	strncpy(threadname, name, sizeof(threadname));
+	strncpy(threadname, name, 16);
 	threadname[16] = 0;
 	prctl (PR_SET_NAME, (unsigned long)threadname);
 }
