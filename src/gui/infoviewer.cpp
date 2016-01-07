11e480b6e88b8e5a675c5b65ffc2b0181f13f831
Thilo Graf <dbt@novatux.de>
2016-01-07 19:33:41 +0100

    CInfoViewer: try to fix channel number allignment
    Allignment was broken on different font size.

--- src/gui/infoviewer.cpp
+++ src/gui/infoviewer.cpp
@@ -777,10 +777,19 @@ void CInfoViewer::showTitle(CZapitChannel * channel, const bool calledFromNumZap
 		if ((!logo_ok && g_settings.infobar_show_channellogo < 2) || g_settings.infobar_show_channellogo == 2 || g_settings.infobar_show_channellogo == 4) // no logo in numberbox
 		{
 			// show number in numberbox
-			int tmpwidth = g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_NUMBER]->getRenderWidth(strChanNum);
-			g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_NUMBER]->RenderString(
-				BoxStartX + (ChanWidth - tmpwidth) / 2, ChanNumYPos,
-				ChanWidth, strChanNum, col_NumBoxText);
+			int h_tmp 	= numbox->getHeight();
+			int y_tmp 	= numbox->getYPos() + 5*100/h_tmp; //5%
+			if (g_settings.infobar_sat_display){
+				int h_sfont = g_SignalFont->getHeight();
+				h_tmp -= h_sfont;
+				y_tmp += h_sfont;
+			}
+			y_tmp += h_tmp/2 + g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_NUMBER]->getHeight()/2;
+			g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_NUMBER]->RenderString(BoxStartX + (ChanWidth-g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_NUMBER]->getRenderWidth(strChanNum))/2,
+											y_tmp,
+											ChanWidth,
+											strChanNum,
+											col_NumBoxText);
 		}
 		if (ChannelLogoMode == 1 || ( g_settings.infobar_show_channellogo == 3 && !logo_ok) || g_settings.infobar_show_channellogo == 6 ) /* channel number besides channel name */
 		{
