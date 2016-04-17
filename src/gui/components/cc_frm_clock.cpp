2b41e1c60fc3c9d7cf912631109849c7028f3834
Thilo Graf <dbt@novatux.de>
2016-04-17 16:21:36 +0200

    CComponentsFrmClock: ensure slot connect, if it is empty

--- src/gui/components/cc_frm_clock.cpp
+++ src/gui/components/cc_frm_clock.cpp
@@ -317,8 +317,10 @@ bool CComponentsFrmClock::startClock()
 
 	if (cl_timer == NULL){
 		cl_timer = new CComponentsTimer();
-		dprintf(DEBUG_INFO, "[CComponentsFrmClock]    [%s]  init slot...\n", __func__);
-		cl_timer->OnTimer.connect(cl_sl);
+		if (cl_timer->OnTimer.empty()){
+			dprintf(DEBUG_INFO,"\033[33m[CComponentsFrmClock]\t[%s] init slot...\033[0m\n", __func__);
+			cl_timer->OnTimer.connect(cl_sl);
+		}
 	}
 	cl_timer->setTimerIntervall(cl_interval);
 
