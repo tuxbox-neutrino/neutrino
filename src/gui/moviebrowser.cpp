fd1c0cfb60e3766ea27b3dbfdb799a11f150bae1
Jacek Jendrzej <crashdvb@googlemail.com>
2016-06-02 22:13:49 +0200

    supplement to d46b287d1cd3448b56ced298c2f0ac46e03472cf

--- src/gui/moviebrowser.cpp
+++ src/gui/moviebrowser.cpp
@@ -1248,7 +1248,11 @@ int CMovieBrowser::paint(void)
 	refreshLCD();
 	if (m_settings.gui == MB_GUI_FILTER)
 		m_settings.gui = MB_GUI_MOVIE_INFO;
-	onSetGUIWindow(m_settings.gui);
+	if (show_mode == MB_SHOW_YT)
+		onSetGUIWindow(MB_GUI_MOVIE_INFO);
+	else
+		onSetGUIWindow(m_settings.gui);
+
 	return (true);
 }
 
