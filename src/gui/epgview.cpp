c0f3e908fffadf46514c2056509b91722622746d
TangoCash <eric@loxat.de>
2016-08-09 17:45:12 +0200

    fix fading from moviebrowser

--- src/gui/epgview.cpp
+++ src/gui/epgview.cpp
@@ -941,6 +941,12 @@ int CEpgData::show(const t_channel_id channel_id, uint64_t a_id, time_t* a_start
 			switch ( msg )
 			{
 			case NeutrinoMessages::EVT_TIMER:
+				if (data == fader.GetFadeTimer()) {
+					if (fader.FadeDone())
+						loop = false;
+				}
+				else
+					CNeutrinoApp::getInstance()->handleMsg(msg, data);
 				if (!mp_info)
 				{
 					if (data == g_InfoViewer->getUpdateTimer()) {
@@ -953,12 +959,6 @@ int CEpgData::show(const t_channel_id channel_id, uint64_t a_id, time_t* a_start
 							pb.paint(false);
 						}
 					}
-					if (data == fader.GetFadeTimer()) {
-						if (fader.FadeDone())
-							loop = false;
-					}
-					else
-						CNeutrinoApp::getInstance()->handleMsg(msg, data);
 				}
 				break;
 			case NeutrinoMessages::EVT_CURRENTNEXT_EPG:
