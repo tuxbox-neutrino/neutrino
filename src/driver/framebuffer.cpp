ed0154a9739ab2c8832fb52336e0e7b454188331
Thilo Graf <dbt@novatux.de>
2016-08-31 10:03:52 +0200

    CFrameBuffer: add debug warning if radius < 0
    Radius < 0 causes for wrong rendering. paintBoxRel() gets confused.
    This will paint partial outside of renderbox.
    
    TODO: This should be fixed.

--- src/driver/framebuffer.cpp
+++ src/driver/framebuffer.cpp
@@ -819,9 +819,11 @@ void CFrameBuffer::paintBoxRel(const int x, const int y, const int dx, const int
 		return;
 
 	if (dx == 0 || dy == 0) {
-		dprintf(DEBUG_NORMAL, "[CFrameBuffer] [%s - %d]: radius %d, start x %d y %d end x %d y %d\n", __FUNCTION__, __LINE__, radius, x, y, x+dx, y+dy);
+		dprintf(DEBUG_NORMAL, "[CFrameBuffer] [%s - %d]: radius %d, start x %d y %d end x %d y %d\n", __func__, __LINE__, radius, x, y, x+dx, y+dy);
 		return;
 	}
+	if (radius < 0)
+		dprintf(DEBUG_NORMAL, "[CFrameBuffer] [%s - %d]: WARNING! radius < 0 [%d] FIXME\n", __func__, __LINE__, radius);
 
 	checkFbArea(x, y, dx, dy, true);
 
