7cdb8a04254399d0e8c942da83bf38d1c0613146
svenhoefer <svenhoefer@svenhoefer.com>
2016-10-19 10:06:36 +0200

    - stringinput: use getItemColors function

--- src/gui/widget/stringinput.cpp
+++ src/gui/widget/stringinput.cpp
@@ -601,19 +601,10 @@ void CStringInput::paintChar(int pos, const char c)
 	fb_pixel_t color;
 	fb_pixel_t bgcolor;
 
-	if (pos == selected)
-	{
-		color   = COL_MENUCONTENTSELECTED_TEXT;
-		bgcolor = COL_MENUCONTENTSELECTED_PLUS_0;
-	}
-	else
-	{
-		color   = COL_MENUCONTENT_TEXT;
-		bgcolor = COL_MENUCONTENT_PLUS_0;
-	}
+	getItemColors(color, bgcolor, pos == selected);
 
-	frameBuffer->paintBoxRel(xpos, ypos, input_w, input_h, COL_MENUCONTENT_PLUS_2);
-	frameBuffer->paintBoxRel(xpos+ 1, ypos+ 1, input_w- 2, input_h- 2, bgcolor);
+	frameBuffer->paintBoxRel(xpos, ypos, input_w, input_h, bgcolor);
+	frameBuffer->paintBoxFrame(xpos, ypos, input_w, input_h, 1, COL_MENUCONTENT_PLUS_2);
 
 	int ch_w = g_Font[SNeutrinoSettings::FONT_TYPE_MENU]->getRenderWidth(ch);
 	int ch_x = xpos + std::max(input_w/2 - ch_w/2, 0);
