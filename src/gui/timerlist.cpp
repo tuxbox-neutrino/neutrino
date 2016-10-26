48c596e2cae1336bee22ecd9dd8f8243c944f530
Jacek Jendrzej <satbaby@kawaii.com>
2016-10-26 18:31:44 +0200

    src/gui/timerlist.cpp update timerlist after show epgview

--- src/gui/timerlist.cpp
+++ src/gui/timerlist.cpp
@@ -934,9 +934,10 @@ int CTimerList::show()
 				if (timer->eventType == CTimerd::TIMER_RECORD || timer->eventType == CTimerd::TIMER_REMOTEBOX || timer->eventType == CTimerd::TIMER_ZAPTO)
 				{
 					hide();
-					if (timer->epgID != 0)
+					if (timer->epgID != 0){
 						res = g_EpgData->show(timer->channel_id, timer->epgID, &timer->epg_starttime);
-					else
+						update=true;
+					}else
 						ShowHint(LOCALE_MESSAGEBOX_INFO, LOCALE_EPGVIEWER_NOTFOUND);
 					if (res==menu_return::RETURN_EXIT_ALL)
 						loop=false;
