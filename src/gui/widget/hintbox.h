3193fef4c6e236c543f582030c1799071a0e8ad7
Thilo Graf <dbt@novatux.de>
2016-11-11 21:31:39 +0100

    CHintbox: enlarge possible max height
    Better use of available screen size for many lines

--- src/gui/widget/hintbox.h
+++ src/gui/widget/hintbox.h
@@ -34,7 +34,7 @@
 
 #define HINTBOX_MIN_WIDTH 420
 #define HINTBOX_MIN_HEIGHT 125
-#define HINTBOX_MAX_HEIGHT 420
+#define HINTBOX_MAX_HEIGHT 520
 #define HINTBOX_DEFAULT_TIMEOUT 5
 //frame around hint container as indent
 #define W_FRAME std::max(HINTBOX_MIN_WIDTH, HINTBOX_MIN_HEIGHT) * 2/100
