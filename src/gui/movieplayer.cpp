2abdd1143ed51dff7a6560e69cfffe7a7f25715f
Jacek Jendrzej <satbaby@kawaii.com>
2017-01-13 18:59:56 +0100

    src/gui/movieplayer.cpp try to fix livestreamInfo

--- src/gui/movieplayer.cpp
+++ src/gui/movieplayer.cpp
@@ -1082,9 +1082,9 @@ void CMoviePlayerGui::stopPlayBack(void)
 		cond.broadcast();
 		pthread_join(bgThread, NULL);
 		bgThread = 0;
+		livestreamInfo1.clear();
+		livestreamInfo2.clear();
 	}
-	livestreamInfo1.clear();
-	livestreamInfo2.clear();
 	printf("%s: stopped\n", __func__);
 }
 
