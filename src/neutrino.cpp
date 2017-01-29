17b8e684e002e0899b1ee906f0b585f0e8c9c536
GetAway <get-away@t-online.de>
2017-01-29 21:27:45 +0100

    - neutrino: fix umount of netfs-drives in ExitRun() function

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -3614,6 +3614,7 @@ void CNeutrinoApp::ExitRun(const bool /*write_si*/, int retcode)
 
 				my_system("/etc/init.d/rcK");
 				sync();
+				CFSMounter::umount(); // unreachable NFS server
 				my_system(2,"/bin/umount", "-a");
 				sleep(1);
 
