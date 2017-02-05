e56fde40828b079e1df3ef457ea3d0a511c34dea
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2017-02-05 21:35:52 +0100

    framebuffer: add accelerator for STi framebuffer
    also add a generic helper class for other accelerated
    framebuffer implementations

---
+++ src/driver/fb_accel.cpp
@@ -0,0 +1,97 @@
+/*
+	Framebuffer acceleration hardware abstraction functions.
+	The infrastructure for hardware dependent framebuffer acceleration
+	functions are implemented in this class, hardware specific stuff
+	is in derived classes.
+
+	(C) 2017 Stefan Seyfried
+
+	License: GPL
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <driver/fb_generic.h>
+#include <driver/fb_accel.h>
+
+CFbAccel::CFbAccel()
+{
+};
+
+CFbAccel::~CFbAccel()
+{
+};
+
+void CFbAccel::paintBoxRel(const int x, const int y, const int dx, const int dy, const fb_pixel_t col, int radius, int type)
+{
+	/* draw a filled rectangle (with additional round corners) */
+	if (!getActive())
+		return;
+
+	checkFbArea(x, y, dx, dy, true);
+
+	if (!type || !radius)
+	{
+		paintRect(x, y, dx, dy, col);
+		checkFbArea(x, y, dx, dy, false);
+		return;
+	}
+
+	setCornerFlags(type);
+	/* limit the radius */
+	radius = limitRadius(dx, dy, radius);
+	if (radius < 1)		/* dx or dy = 0... */
+		radius = 1;	/* avoid div by zero below */
+
+	int line = 0;
+	while (line < dy) {
+		int ofl, ofr;
+		if (calcCorners(NULL, &ofl, &ofr, dy, line, radius, type)) {
+			int height = dy - ((corner_tl || corner_tr)?radius: 0 ) - ((corner_bl || corner_br) ? radius : 0);
+			paintRect(x, y + line, dx, height, col);
+			line += height;
+			continue;
+		}
+		if (dx - ofr - ofl < 1) {
+			//printf("FB-NG::%s:%d x %d y %d dx %d dy %d l %d r %d\n", __func__, __LINE__, x,y,dx,dy, ofl, ofr);
+			line++;
+			continue;
+		}
+		paintLine(x + ofl, y + line, x + dx - ofr, y + line, col);
+		line++;
+	}
+	checkFbArea(x, y, dx, dy, false);
+	mark(x, y, x+dx, y+dy);
+}
+
+void CFbAccel::paintRect(const int x, const int y, const int dx, const int dy, const fb_pixel_t col)
+{
+	int line = 0;
+	int swidth = stride / sizeof(fb_pixel_t);
+	fb_pixel_t *fbp = getFrameBufferPointer() + (swidth * y);
+	int pos;
+	while (line < dy)
+	{
+		for (pos = x; pos < x + dx; pos++)
+			*(fbp + pos) = col;
+		fbp += swidth;
+		line++;
+	}
+	mark(x, y, x+dx, y+dy);
+	blit();
+}
---
+++ src/driver/fb_accel.h
@@ -0,0 +1,81 @@
+/*
+	Copyright (C) 2007-2013 Stefan Seyfried
+
+	License: GPL
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+	private functions for the fbaccel class (only used in CFrameBuffer)
+*/
+
+
+#ifndef __fbaccel__
+#define __fbaccel__
+#include <config.h>
+#include <OpenThreads/Mutex>
+#include <OpenThreads/ScopedLock>
+#include <OpenThreads/Thread>
+#include <OpenThreads/Condition>
+#include "fb_generic.h"
+
+#if HAVE_SPARK_HARDWARE
+#define PARTIAL_BLIT 1
+#endif
+
+class CFbAccel
+	: public CFrameBuffer
+{
+	public:
+		CFbAccel();
+		~CFbAccel();
+		void paintBoxRel(const int x, const int y, const int dx, const int dy, const fb_pixel_t col, int radius, int type);
+		virtual void paintRect(const int x, const int y, const int dx, const int dy, const fb_pixel_t col);
+};
+
+class CFbAccelSTi
+	: public OpenThreads::Thread, public CFbAccel
+{
+	private:
+		void run(void);
+		void blit(void);
+		void _blit(void);
+		bool blit_thread;
+		bool blit_pending;
+		OpenThreads::Condition blit_cond;
+		OpenThreads::Mutex blit_mutex;
+		fb_pixel_t *backbuffer;
+#ifdef PARTIAL_BLIT
+		struct {
+			int xs;
+			int ys;
+			int xe;
+			int ye;
+		} to_blit;
+		uint32_t last_xres;
+#endif
+	public:
+		CFbAccelSTi();
+		~CFbAccelSTi();
+		void init(const char * const);
+		int setMode(unsigned int xRes, unsigned int yRes, unsigned int bpp);
+		void paintRect(const int x, const int y, const int dx, const int dy, const fb_pixel_t col);
+		void blit2FB(void *fbbuff, uint32_t width, uint32_t height, uint32_t xoff, uint32_t yoff, uint32_t xp, uint32_t yp, bool transp);
+		void waitForIdle(const char *func = NULL);
+		void mark(int x, int y, int dx, int dy);
+		fb_pixel_t * getBackBufferPointer() const;
+		void setBlendMode(uint8_t);
+		void setBlendLevel(int);
+};
+
+#endif
