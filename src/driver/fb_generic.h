e8ec4540ca3ed77c4ce59c145609d13dbf7b1ca6
M. Liebmann <git@tuxcode.de>
2017-02-14 19:57:28 +0100

    fb_generic: make blitBox2FB virtual

--- src/driver/fb_generic.h
+++ src/driver/fb_generic.h
@@ -267,7 +267,7 @@ class CFrameBuffer : public sigc::trackable
 		void* convertRGBA2FB(unsigned char *rgbbuff, unsigned long x, unsigned long y);
 		void displayRGB(unsigned char *rgbbuff, int x_size, int y_size, int x_pan, int y_pan, int x_offs, int y_offs, bool clearfb = true, int transp = 0xFF);
 		virtual void blit2FB(void *fbbuff, uint32_t width, uint32_t height, uint32_t xoff, uint32_t yoff, uint32_t xp = 0, uint32_t yp = 0, bool transp = false);
-		void blitBox2FB(const fb_pixel_t* boxBuf, uint32_t width, uint32_t height, uint32_t xoff, uint32_t yoff);
+		virtual void blitBox2FB(const fb_pixel_t* boxBuf, uint32_t width, uint32_t height, uint32_t xoff, uint32_t yoff);
 
 		virtual void mark(int x, int y, int dx, int dy);
 
