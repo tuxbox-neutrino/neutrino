0e435bb389d3923325d8f92bb452e45268e88966
svenhoefer <svenhoefer@svenhoefer.com>
2017-02-22 08:59:56 +0100

    - rc_input: rename checkLnkDev() to checkdev_lnk(); ...
    just to align form-style to latest changes

--- src/driver/rcinput.h
+++ src/driver/rcinput.h
@@ -171,7 +171,7 @@ class CRCInput
 		bool checkpath(in_dev id);
 		bool checkdev();
 #ifdef BOXMODEL_CS_HD2
-		bool checkLnkDev(std::string lnk);
+		bool checkdev_lnk(std::string lnk);
 #endif
 		void close();
 		int translate(int code);
