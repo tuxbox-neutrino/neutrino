3d7f6f13c4646bddd536b6d119c217396c7ce488
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2017-02-22 20:05:54 +0100

    Revert "- rc_input: just cosmetics to satisfy syntax-highlighters"
    This reverts commit d7ed439e53cc55b5c936fe843044ff8fc708b17d.

--- src/driver/rcinput.cpp
+++ src/driver/rcinput.cpp
@@ -575,6 +575,8 @@ int CRCInput::checkTimers()
 	return _id;
 }
 
+
+
 int64_t CRCInput::calcTimeoutEnd(const int timeout_in_seconds)
 {
 	return time_monotonic_us() + ((uint64_t)timeout_in_seconds * (uint64_t) 1000000);
@@ -1427,13 +1429,13 @@ void CRCInput::getMsg_us(neutrino_msg_t * msg, neutrino_msg_data_t * data, uint6
 					if (trkey == rc_last_key) {
 						/* only allow selected keys to be repeated */
 						if (mayRepeat(trkey, bAllowRepeatLR) ||
-						    (g_settings.shutdown_real_rcdelay && ((trkey == RC_standby) &&
+						    (g_settings.shutdown_real_rcdelay &&
+						    ((trkey == RC_standby) &&
 #if HAVE_COOL_HARDWARE
-						    (cs_get_revision() > 7)
+						    (cs_get_revision() > 7))))
 #else
-						    (g_info.hw_caps->can_shutdown)
+						    (g_info.hw_caps->can_shutdown))))
 #endif
-						)))
 						{
 #ifdef ENABLE_REPEAT_CHECK
 							if (rc_last_repeat_key != trkey) {
