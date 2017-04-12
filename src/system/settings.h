8bfee0b7dcdd689c953019db733909a64497f283
svenhoefer <svenhoefer@svenhoefer.com>
2017-04-12 13:28:54 +0200

    - settings: fix calculation for SCROLLBAR_WIDTH ...
    to work with subtractions too

--- src/system/settings.h
+++ src/system/settings.h
@@ -920,7 +920,7 @@ const time_settings_struct_t timing_setting[SNeutrinoSettings::TIMING_SETTING_CO
 #define OFFSET_INNER_MIN	2
 #define OFFSET_INNER_NONE	0
 
-#define SCROLLBAR_WIDTH		OFFSET_INNER_MID + 2*OFFSET_INNER_MIN
+#define SCROLLBAR_WIDTH		(OFFSET_INNER_MID + 2*OFFSET_INNER_MIN)
 
 #define DETAILSLINE_WIDTH	16 // TODO: scale2Res() ?
 
