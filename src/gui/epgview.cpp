17958da28fc32fc857066000456e0c6af90099e7
Thilo Graf <dbt@novatux.de>
2017-05-04 19:45:11 +0200

    CEpgData: supplement to 'fix header size'
    commit: 7583714e6703a7ae9591e112726759c4070d0c68
    
    Permanent font init is required to avoid possible crash after
    changed font settings by user.

--- src/gui/epgview.cpp
+++ src/gui/epgview.cpp
@@ -132,7 +132,7 @@ CEpgData::CEpgData()
 	lText 		= NULL;
 	rText		= NULL;
 	pb		= NULL;
-	font_title	= g_Font[SNeutrinoSettings::FONT_TYPE_EPG_TITLE];
+	font_title	= NULL;
 }
 
 CEpgData::~CEpgData()
@@ -145,6 +145,7 @@ void CEpgData::start()
 	ox = frameBuffer->getScreenWidthRel(bigFonts ? false /* big */ : true /* small */);
 	oy = frameBuffer->getScreenHeightRel(bigFonts ? false /* big */ : true /* small */);
 
+	font_title   = g_Font[SNeutrinoSettings::FONT_TYPE_EPG_TITLE];
 	topheight    = font_title->getHeight();
 	topboxheight = topheight + 6;
 	botboxheight = g_Font[SNeutrinoSettings::FONT_TYPE_EPG_DATE]->getHeight() + 6;
@@ -849,13 +850,13 @@ int CEpgData::show(const t_channel_id channel_id, uint64_t a_id, time_t* a_start
 	// header + logo
 	if (!header){
 		header = new CComponentsHeader(sx, sy, ox, toph);
-		header->setCaptionFont(font_title);
 		header->setColorBody(COL_MENUHEAD_PLUS_0);
 		header->enableColBodyGradient(g_settings.theme.menu_Head_gradient, COL_MENUCONTENT_PLUS_0, g_settings.theme.menu_Head_gradient_direction);
 		header->enableClock(true, "%H:%M", "%H %M", true);
 	}else
 		header->setDimensionsAll(sx, sy, ox, toph);
 
+	header->setCaptionFont(font_title);
 	header->setCaption(epgData.title);
 
 	if (header->isPainted())
