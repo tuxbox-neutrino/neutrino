e3cc6cee67f5314efb0222d533d76001a60ea686
M. Liebmann <git@tuxcode.de>
2017-06-16 19:10:23 +0200

    Revert "HD1: Workaround for missing cs_get_chip_type"
    This reverts commit a6fe60d60a5363973459384a156ed49c9e9ee91c.

--- lib/hardware/coolstream/hardware_caps.cpp
+++ lib/hardware/coolstream/hardware_caps.cpp
@@ -6,7 +6,6 @@
  *
  * License: GPL v2 or later
  */
-#include <config.h>
 #include "cs_api.h"
 #include <stdio.h>
 #include <string.h>
@@ -21,10 +20,7 @@ hw_caps_t *get_hwcaps(void) {
 	if (initialized)
 		return &caps;
 	int rev = cs_get_revision();
-	int chip = 0;
-#ifdef BOXMODEL_CS_HD2
-	chip = cs_get_chip_type();
-#endif
+	int chip = cs_get_chip_type();
 	caps.has_fan = (rev < 8 && CFEManager::getInstance()->getFE(0)->hasSat()); // only SAT-HD1 before rev 8 has fan
 	caps.has_HDMI = 1;
 	caps.has_SCART = (rev != 10);
