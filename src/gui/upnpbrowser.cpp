baf441b5d074b60c17b1fd829ea86504aa340dd6
Thilo Graf <dbt@novatux.de>
2017-07-01 20:40:20 +0200

    CUpnpBrowserGui: fix dline paint during directory selection
    Dline was not removed on directory selection.

--- src/gui/upnpbrowser.cpp
+++ src/gui/upnpbrowser.cpp
@@ -1226,8 +1226,11 @@ void CUpnpBrowserGui::paintDetails(UPnPEntry *entry, bool use_playing)
 
 void CUpnpBrowserGui::paintItem2DetailsLine(int pos)
 {
-	if (pos < 0)
+	if (pos < 0){
+		if (dline)
+			dline->kill();
 		return;
+	}
 
 	int xpos  = m_x - DETAILSLINE_WIDTH;
 	int ypos1 = m_item_y + pos*m_item_height;
