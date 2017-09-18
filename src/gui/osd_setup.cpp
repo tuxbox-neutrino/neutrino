82a0cdeb685035d99e6e6d75f1e572a50ecc85d0
Jacek Jendrzej <satbaby@kawaii.com>
2017-09-18 14:57:11 +0200

    osd_setup.cpp fix memleak

--- src/gui/osd_setup.cpp
+++ src/gui/osd_setup.cpp
@@ -1516,6 +1516,7 @@ int COsdSetup::showContextChanlistMenu(CChannelList *parent_channellist)
 
 	int res = menu_chanlist->exec(NULL, "");
 	cselected = menu_chanlist->getSelected();
+	delete channellistNotifier;
 	delete menu_chanlist;
 	return res;
 }
