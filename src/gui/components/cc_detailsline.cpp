95ab4ac708392c355def7821a7675b5bbb47d05e
svenhoefer <svenhoefer@svenhoefer.com>
2017-09-25 08:42:26 +0200

    - detailsline: reduce two times the shadow width from the h_marks
    Otherwise shadows will be painted lower then the height of the object
    the detailsline is docked to.

--- src/gui/components/cc_detailsline.cpp
+++ src/gui/components/cc_detailsline.cpp
@@ -53,12 +53,14 @@ void CComponentsDetailsLine::initVarDline(	const int& x_pos, const int& y_pos_to
 	col_shadow	= color_shadow;
 	col_body	= color_line;
 
+	shadow_w	= 1;
+
 	//CComponentsDetailsLine
 	y_down 		= y_pos_down;
-	h_mark_top 	= h_mark_top_;
-	h_mark_down 	= h_mark_down_;
 
-	shadow_w	= 1;
+	// reduce two times the shadow width, to avoid shadow overlaps
+	h_mark_top 	= h_mark_top_ - 2*shadow_w;
+	h_mark_down 	= h_mark_down_ - 2*shadow_w;
 
 	//CComponentsDetailsLine
 	dl_w 	= CFrameBuffer::getInstance()->scale2Res(3);
