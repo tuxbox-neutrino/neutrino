ca09c92542971474cf21634b31a088c70635638d
Jacek Jendrzej <satbaby@kawaii.com>
2017-09-26 12:39:36 +0200

    Revert "src/gui/osd_setup.cpp disable hint paint in channellist-mode, is broken"
    This reverts commit e308407b229b72cf2f1851a122221878114033d1.

--- src/gui/osd_setup.h
+++ src/gui/osd_setup.h
@@ -71,7 +71,7 @@ class COsdSetup : public CMenuTarget, public CChangeObserver
 		void showOsdTimeoutSetup(CMenuWidget *menu_timeout);
 		void showOsdMenusSetup(CMenuWidget *menu_menus);
 		void showOsdInfobarSetup(CMenuWidget *menu_infobar);
-		void showOsdChanlistSetup(CMenuWidget *menu_chanlist, bool hint_paint = true);
+		void showOsdChanlistSetup(CMenuWidget *menu_chanlist);
 		void showOsdEventlistSetup(CMenuWidget *menu_eventlist);
 		void showOsdVolumeSetup(CMenuWidget *menu_volume);
 		void showOsdInfoclockSetup(CMenuWidget *menu_infoclock);
