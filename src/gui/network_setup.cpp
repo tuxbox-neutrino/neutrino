db5af2d9caa573cc30b0906431e67690a16a1919
svenhoefer <svenhoefer@svenhoefer.com>
2017-09-27 08:32:42 +0200

    - network-setup: add header-icon to network-test

--- src/gui/network_setup.cpp
+++ src/gui/network_setup.cpp
@@ -787,7 +787,7 @@ void CNetworkSetup::testNetworkSettings()
 		}
 	}
 
-	ShowMsg(LOCALE_NETWORKMENU_TEST, text, CMsgBox::mbrBack, CMsgBox::mbBack, NULL, MSGBOX_MIN_WIDTH, NO_TIMEOUT, false, CMsgBox::AUTO_WIDTH | CMsgBox::AUTO_HIGH); // UTF-8
+	ShowMsg(LOCALE_NETWORKMENU_TEST, text, CMsgBox::mbrBack, CMsgBox::mbBack, NEUTRINO_ICON_NETWORK, MSGBOX_MIN_WIDTH, NO_TIMEOUT, false, CMsgBox::AUTO_WIDTH | CMsgBox::AUTO_HIGH);
 }
 
 int CNetworkSetup::showWlanList()
