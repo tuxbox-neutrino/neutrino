4ad5ce65e2dec80eb25fd948aa229e41816bf25f
Thilo Graf <dbt@novatux.de>
2017-10-08 17:48:32 +0200

    CMenuWidget: small clean ups

--- src/gui/widget/menue.cpp
+++ src/gui/widget/menue.cpp
@@ -1455,10 +1455,8 @@ void CMenuWidget::enableSaveScreen(bool enable)
 
 void CMenuWidget::paintHint(int pos)
 {
-	if (!g_settings.show_menu_hints){
-		//ResetModules(); //ensure clean up on changed setting
+	if (!g_settings.show_menu_hints)
 		return;
-	}
 
 	if (pos < 0 && !hint_painted)
 		return;
