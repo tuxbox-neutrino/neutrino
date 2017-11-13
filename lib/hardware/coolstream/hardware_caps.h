1198764ef276652989dca8c6ec2ba70b102c2316
Thilo Graf <dbt@novatux.de>
2017-11-13 20:58:06 +0100

    coolstream/hardware_caps.h: fix broken cs build
    'display_has_statusline' was missed with commit:
    f6093052466d9da01db74315a07bf82c249838ca

--- lib/hardware/coolstream/hardware_caps.h
+++ lib/hardware/coolstream/hardware_caps.h
@@ -35,6 +35,7 @@ typedef struct hw_caps
 	int display_yres;
 	int display_can_deepstandby;
 	int display_can_set_brightness;
+	int display_has_statusline;
 	char boxvendor[64];
 	char boxname[64];
 	char boxarch[64];
