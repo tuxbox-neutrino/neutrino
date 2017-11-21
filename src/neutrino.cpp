a857be6cc382553b3888b45cc8a856ab75fa8942
svenhoefer <svenhoefer@svenhoefer.com>
2017-11-21 11:58:25 +0100

    - neutrino: deactivate compatibility code in ExitRun(); ...
    our start script is aligned to new exit code 2
    see https://bitbucket.org/neutrino-images/ni-buildsystem/commits/cd1328c41cb642c141b58d001dce06a96eff0a1c
    
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -3811,6 +3811,7 @@ void CNeutrinoApp::ExitRun(int exit_code)
 	if (!do_exiting)
 		return;
 
+#if 0
 	/*
 	   For compatibility: /tmp/.reboot is not really needed anymore
 	   if we use the defined exit code 2 instead of this flagfile.
@@ -3824,6 +3825,7 @@ void CNeutrinoApp::ExitRun(int exit_code)
 	}
 	else
 		unlink("/tmp/.reboot");
+#endif
 
 	printf("[neutrino] %s(int %d)\n", __func__, exit_code);
 	printf("[neutrino] hw_caps->can_shutdown: %d\n", g_info.hw_caps->can_shutdown);
