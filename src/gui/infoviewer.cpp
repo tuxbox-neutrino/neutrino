87aeec938f284bef8c8a89886010d89a94ea645c
svenhoefer <svenhoefer@svenhoefer.com>
2018-01-09 11:14:08 +0100

    - infoviewer: fix indentation in setInfobarTimeout()
     Conflicts:
    	src/gui/infoviewer.cpp
    
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/gui/infoviewer.cpp
+++ src/gui/infoviewer.cpp
@@ -914,6 +914,7 @@ void CInfoViewer::showTitle(CZapitChannel * channel, const bool calledFromNumZap
 	aspectRatio = 0;
 	fileplay = 0;
 }
+
 void CInfoViewer::setInfobarTimeout(int timeout_ext)
 {
 	int mode = CNeutrinoApp::getInstance()->getMode();
@@ -922,19 +923,19 @@ void CInfoViewer::setInfobarTimeout(int timeout_ext)
 	{
 		case NeutrinoModes::mode_radio:
 		case NeutrinoModes::mode_webradio:
-				timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR_RADIO] + timeout_ext);
-				break;
+			timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR_RADIO] + timeout_ext);
+			break;
 		case NeutrinoModes::mode_ts:
-				if (CMoviePlayerGui::getInstance().IsAudioPlaying())
-					timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR_MEDIA_AUDIO] + timeout_ext);
-				else
-					timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR_MEDIA_VIDEO] + timeout_ext);
-				break;
+			if (CMoviePlayerGui::getInstance().IsAudioPlaying())
+				timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR_MEDIA_AUDIO] + timeout_ext);
+			else
+				timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR_MEDIA_VIDEO] + timeout_ext);
+			break;
 		case NeutrinoModes::mode_tv:
 		case NeutrinoModes::mode_webtv:
 		default:
-				timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR] + timeout_ext);
-				break;
+			timeoutEnd = CRCInput::calcTimeoutEnd(g_settings.timing[SNeutrinoSettings::TIMING_INFOBAR] + timeout_ext);
+			break;
 	}
 }
 
