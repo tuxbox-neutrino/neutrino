ddf3bfd613e132de6d8eab2000356f126632038a
svenhoefer <svenhoefer@svenhoefer.com>
2018-01-19 19:47:41 +0100

    - epgview: fix logic for followscreenings- and tmdb-button
    Signed-off-by: Thilo Graf <dbt@novatux.de>
    
    include c/p fix

--- src/gui/epgview.cpp
+++ src/gui/epgview.cpp
@@ -1621,28 +1621,27 @@ void CEpgData::showTimerEventBar (bool pshow, bool adzap, bool mp_info)
 
 	if (mp_info)
 	{
-		if (!g_settings.tmdb_enabled)
-		{
-			// disable tmdb button
+		// check tmdb button
+		if (g_settings.tmdb_enabled)
+			EpgButtons[MP_BUTTONS][2].button = NEUTRINO_ICON_BUTTON_0;
+		else
 			EpgButtons[MP_BUTTONS][2].button = NEUTRINO_ICON_BUTTON_DUMMY_SMALL;
-		}
 
 		::paintButtons(x, y, w, MaxButtons, EpgButtons[MP_BUTTONS], w, h);
 	}
 	else
 	{
-		bool fscr = (has_follow_screenings && !call_fromfollowlist);
-		if (!fscr)
-		{
-			// disable followscreenings button
+		// check followscreenings button
+		if (has_follow_screenings && !call_fromfollowlist)
+			EpgButtons[TV_BUTTONS][3].button = NEUTRINO_ICON_BUTTON_BLUE;
+		else
 			EpgButtons[TV_BUTTONS][3].button = NEUTRINO_ICON_BUTTON_DUMMY_SMALL;
-		}
 
-		if (!g_settings.tmdb_enabled)
-		{
-			// disable tmdb button
+		// check tmdb button
+		if (g_settings.tmdb_enabled)
+			EpgButtons[TV_BUTTONS][4].button = NEUTRINO_ICON_BUTTON_0;
+		else
 			EpgButtons[TV_BUTTONS][4].button = NEUTRINO_ICON_BUTTON_DUMMY_SMALL;
-		}
 
 		if (g_settings.recording_type != CNeutrinoApp::RECORDING_OFF)
 			::paintButtons(x, y, w, MaxButtons, EpgButtons[TV_BUTTONS], w, h, "", false, COL_MENUFOOT_TEXT, adzap ? adzap_button.c_str() : NULL, 2);
