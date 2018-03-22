2f73bd58a1a6339e79f5e4607236386bec6f1f49
Thilo Graf <dbt@novatux.de>
2018-03-22 21:10:47 +0100

    CNeutrinoApp: ensure clean up of screen in standby mode
    This should fix a possible issue with infobar artefacts in standby mode.
    
    This issue was observed if user went to change  to standby mode and
    infobar was still open e.g if  infobar timeout was set to 0
    (always visible) and runtime timeout was elapsed.

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -4120,8 +4120,6 @@ void CNeutrinoApp::standbyMode( bool bOnOff, bool fromDeepStandby )
 			SDT_ReloadChannels();
 			//SDTreloadChannels = false;
 		}
-		frameBuffer->useBackground(false);
-		frameBuffer->paintBackground();
 
 		/* wasshift = */ CRecordManager::getInstance()->StopAutoRecord();
 
@@ -4178,7 +4176,12 @@ void CNeutrinoApp::standbyMode( bool bOnOff, bool fromDeepStandby )
 		if (g_info.hw_caps->has_fan)
 			CFanControlNotifier::setSpeed(1);
 
+		if (g_InfoViewer->is_visible)
+			g_InfoViewer->killTitle();
+		frameBuffer->useBackground(false);
+		frameBuffer->paintBackground();
 		frameBuffer->setActive(false);
+
 		// Active standby on
 		powerManager->SetStandby(false, false);
 		if (scansettings.fst_update)
@@ -4206,7 +4209,9 @@ void CNeutrinoApp::standbyMode( bool bOnOff, bool fromDeepStandby )
 			g_CamHandler->exec(NULL, "ca_ci_reset0");
 			g_CamHandler->exec(NULL, "ca_ci_reset1");
 		}
+
 		frameBuffer->setActive(true);
+
 		//fan speed
 		if (g_info.hw_caps->has_fan)
 			CFanControlNotifier::setSpeed(g_settings.fan_speed);
