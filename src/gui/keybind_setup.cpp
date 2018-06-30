14384c8ad62ef8014f098a71e58d04d4d332d9d8
svenhoefer <svenhoefer@svenhoefer.com>
2018-06-30 20:05:35 +0200

    - keybind-setup: try to fix return value of msgbox when new rc is not confirmed
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/gui/keybind_setup.cpp
+++ src/gui/keybind_setup.cpp
@@ -330,7 +330,7 @@ int CKeybindSetup::showKeySetup()
 		strcat(RC_HW_msg, g_Locale->getText(LOCALE_KEYBINDINGMENU_REMOTECONTROL_HARDWARE_MSG_PART2));
 		strcat(RC_HW_msg, RC_HW_str[g_settings.remote_control_hardware]);
 		strcat(RC_HW_msg, g_Locale->getText(LOCALE_KEYBINDINGMENU_REMOTECONTROL_HARDWARE_MSG_PART3));
-		if(ShowMsg(LOCALE_MESSAGEBOX_INFO, RC_HW_msg, CMsgBox::mbrNo, CMsgBox::mbYes | CMsgBox::mbNo, NEUTRINO_ICON_INFO, 450, 15) == CMsgBox::mbrNo) {
+		if (ShowMsg(LOCALE_MESSAGEBOX_INFO, RC_HW_msg, CMsgBox::mbrNo, CMsgBox::mbYes | CMsgBox::mbNo, NEUTRINO_ICON_INFO, 450, 15, true) == CMsgBox::mbrNo) {
 			g_settings.remote_control_hardware = org_remote_control_hardware;
 			g_RCInput->CRCInput::set_rc_hw();
 		}
