b9298eb4aca0730cd3a52d6ead4e8ca507f7f3fe
TangoCash <eric@loxat.de>
2018-09-09 13:06:05 +0200

    read logo marker in m3u files
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/zapit/src/bouquets.cpp
+++ src/zapit/src/bouquets.cpp
@@ -953,10 +953,11 @@ void CBouquetManager::loadWebchannels(int mode)
 			{
 				std::ifstream infile;
 				char cLine[1024];
-				std::string desc;
+				std::string desc = "";
 				std::string title = "";
 				std::string group = "";
 				std::string epgid = "";
+				std::string alogo = "";
 				CZapitBouquet* pbouquet = NULL;
 
 				infile.open(tmp_name.c_str(), std::ifstream::in);
@@ -982,6 +983,7 @@ void CBouquetManager::loadWebchannels(int mode)
 						title = "";
 						group = "";
 						desc = "";
+						alogo = "";
 
 						if (iColon >= 0 && iComma >= 0 && iComma > iColon)
 						{
@@ -992,6 +994,7 @@ void CBouquetManager::loadWebchannels(int mode)
 							desc = ReadMarkerValue(strInfoLine, TVG_INFO_NAME_MARKER);
 							group = ReadMarkerValue(strInfoLine, GROUP_NAME_MARKER);
 							epgid = ReadMarkerValue(strInfoLine, TVG_INFO_ID_MARKER);
+							alogo = ReadMarkerValue(strInfoLine, TVG_INFO_LOGO_MARKER);
 						}
 
 						pbouquet = addBouquetIfNotExist((mode == MODE_WEBTV) ? "WebTV" : "WebRadio");
@@ -1033,6 +1036,8 @@ void CBouquetManager::loadWebchannels(int mode)
 								}
 								CZapitChannel * channel = new CZapitChannel(title.c_str(), chid, url, desc.c_str(), chid, epg_script.c_str(), mode);
 								CServiceManager::getInstance()->AddChannel(channel);
+								if (!alogo.empty())
+									channel->setAlternateLogo(dlTmpName(alogo));
 								channel->flags = CZapitChannel::UPDATED;
 								if (gbouquet)
 									gbouquet->addService(channel);
