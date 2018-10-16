ffbb24609c9d1aaf50d37205df7e9305e3f0e512
Thilo Graf <dbt@novatux.de>
2018-10-16 16:49:52 +0200

    helpers.cpp: reduce debug output in downloadUrl()
    Usage of debug level INFO should be enough.

--- src/system/helpers.cpp
+++ src/system/helpers.cpp
@@ -1652,7 +1652,7 @@ bool getUrl(std::string& url, std::string& answer, std::string userAgent, unsign
 
 bool downloadUrl(std::string url, std::string file, std::string userAgent, unsigned int timeout)
 {
-	dprintf(DEBUG_NORMAL ,"downloadUrl: url:%s file:%s userAgent:%s\n", url.c_str(), file.c_str(), userAgent.c_str());
+	dprintf(DEBUG_INFO ,"[%s - %d] url: %s\tfile: %s\tuserAgent: %s\n", __func__, __LINE__, url.c_str(), file.c_str(), userAgent.c_str());
 
 	CURL * curl_handle = curl_easy_init();
 
