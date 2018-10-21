b902f870e66a1bdac5d0d15cf534dad8c1c188e8
Thilo Graf <dbt@novatux.de>
2018-10-21 18:37:25 +0200

    src/gui/screensaver.cpp: use showFrame() instead paintImage()
    showFrame now seems to work fine inside screensaver

--- src/gui/screensaver.cpp
+++ src/gui/screensaver.cpp
@@ -297,7 +297,7 @@ void CScreenSaver::paint()
 		}
 
 		dprintf(DEBUG_INFO, "[CScreenSaver]  %s - %d : %s\n",  __func__, __LINE__, v_bg_files.at(index).c_str());
-		paintImage(v_bg_files.at(index), 0, 0, m_frameBuffer->getScreenWidth(true), m_frameBuffer->getScreenHeight(true));
+		m_frameBuffer->showFrame(v_bg_files.at(index));
 
 		if (!g_settings.screensaver_random)
 			index++;
