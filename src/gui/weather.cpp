edc597748dd276d4d27da5102046377f9f0bb16e
svenhoefer <svenhoefer@svenhoefer.com>
2019-03-24 22:57:49 +0100

    - weather: use km/h instead of m/h
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/gui/weather.cpp
+++ src/gui/weather.cpp
@@ -96,7 +96,7 @@ bool CWeather::GetWeatherDetails()
 	if (!g_settings.weather_enabled)
 		return false;
 
-	std::string data = "https://api.darksky.net/forecast/" + key + "/" + coords + "?units=si&lang=de&exclude=minutely,hourly,flags,alerts";
+	std::string data = "https://api.darksky.net/forecast/" + key + "/" + coords + "?units=ca&lang=de&exclude=minutely,hourly,flags,alerts";
 	JSONCPP_STRING answer;
 	JSONCPP_STRING formattedErrors;
 
