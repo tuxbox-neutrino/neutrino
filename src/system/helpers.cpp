cca09c7469ba9116a1c66882e71b5df78118a39c
Thilo Graf <dbt@novatux.de>
2019-03-28 11:28:47 +0100

    helpers: add trim methodes for left and right trim

--- src/system/helpers.cpp
+++ src/system/helpers.cpp
@@ -483,6 +483,18 @@ std::string trim(std::string &str, const std::string &trimChars /*= " \n\r\t"*/)
 	return result.erase(0, result.find_first_not_of(trimChars));
 }
 
+std::string ltrim(std::string& str, const std::string& trimChars)
+{
+	str.erase(0, str.find_first_not_of(trimChars));
+	return str;
+}
+
+std::string rtrim(std::string& str, const std::string& trimChars)
+{
+    str.erase(str.find_last_not_of(trimChars) + 1);
+    return str;
+}
+
 std::string cutString(const std::string str, int msgFont, const int width)
 {
 	Font *msgFont_ = g_Font[msgFont];
