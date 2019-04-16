df19fff8748bf8a3dcf315981332dbc7802cdab5
svenhoefer <svenhoefer@svenhoefer.com>
2019-04-16 23:42:51 +0200

    - autogen.sh: small cosmetics
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- autogen.sh
+++ autogen.sh
@@ -6,10 +6,10 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 cd "$srcdir"
-echo "Generating configuration files for $package, please wait...."
+echo "Generating configuration files for $package, please wait ..."
 
 aclocal --force
 libtoolize --force
 autoconf --force
-autoheader  --force
+autoheader --force
 automake --add-missing --force-missing
