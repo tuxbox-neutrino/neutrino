14b96ccc9513a4a2b6e139afdb2d2fe50b843dda
Jacek Jendrzej <overx300@gmail.com>
2019-05-04 14:54:34 +0200

    eventlist: fix memleak

--- src/gui/eventlist.cpp
+++ src/gui/eventlist.cpp
@@ -148,6 +148,7 @@ void CEventList::UpdateTimerList(void)
 	CTimerList *Timerlist = new CTimerList;
 	Timerlist->RemoteBoxTimerList(timerlist);
 	sort(timerlist.begin(), timerlist.end());
+	delete Timerlist;
 }
 
 // Function: HasTimerConflicts
