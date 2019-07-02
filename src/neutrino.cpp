8e5292abc31ee100a09a378d141cbb87a12a1d08
Thilo Graf <dbt@novatux.de>
2019-07-02 12:26:08 +0200

    neutrino.cpp: fix memleak with progressbar cache
    detected with LeakSanitizer

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -108,7 +108,7 @@
 #endif
 #include "gui/themes.h"
 #include "gui/timerlist.h"
-
+#include "gui/components/cc_item_progressbar.h"
 #include <system/ytcache.h>
 
 #include <hardware/audio.h>
@@ -4115,6 +4115,9 @@ void CNeutrinoApp::ExitRun(int exit_code)
 	dprintf(DEBUG_INFO, "exit\n");
 	OnShutDown();
 
+	//cleanup progress bar cache
+	CProgressBarCache::pbcClear();
+
 	StopSubtitles();
 	stopPlayBack();
 
@@ -4723,6 +4726,9 @@ int CNeutrinoApp::exec(CMenuTarget* parent, const std::string & actionKey)
 	else if(actionKey=="restart") {
 		//usage of slots from any classes
 		OnBeforeRestart();
+
+		//cleanup progress bar cache
+		CProgressBarCache::pbcClear();
 
 		if (recordingstatus)
 			DisplayErrorMessage(g_Locale->getText(LOCALE_SERVICEMENU_RESTART_REFUSED_RECORDING));
