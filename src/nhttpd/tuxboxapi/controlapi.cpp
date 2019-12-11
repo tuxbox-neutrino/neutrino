1b72223cab05dd27c8a630a40d5e156edce37254
svenhoefer <svenhoefer@svenhoefer.com>
2019-12-11 22:19:09 +0100

    - controlapi: addition to commit dcfb6a1ebdc5219e22329657b2f5600d098de634
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/nhttpd/tuxboxapi/controlapi.cpp
+++ src/nhttpd/tuxboxapi/controlapi.cpp
@@ -895,13 +895,15 @@ unsigned int revert_translate(unsigned int code)
 		case RC_page_down:
 			return KEY_CHANNELDOWN;
 #ifdef HAVE_ARM_HARDWARE
+#if BOXMODEL_HD51 || BOXMODEL_HD60 || BOXMODEL_HD61 || BOXMODEL_BRE2ZE4K || BOXMODEL_H7
+		case RC_favorites:
+			return KEY_VIDEO;
+#endif
 		case RC_mode:
 			return KEY_SWITCHVIDEOMODE;
 		case RC_play:
 		case RC_pause:
 			return KEY_PLAYPAUSE;
-		case RC_favorites:
-			return KEY_VIDEO;
 		case RC_forward:
 			return KEY_FASTFORWARD;
 #endif
