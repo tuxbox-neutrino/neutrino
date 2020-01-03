f9314ec1096944dfce69af775755165c4c110f1b
Jacek Jendrzej <overx300@gmail.com>
2020-01-03 17:25:35 +0100

    fix last commit;  webtv_setup fix scandir memleak

--- src/gui/webtv_setup.cpp
+++ src/gui/webtv_setup.cpp
@@ -375,10 +375,10 @@ void CWebTVSetup::webchannels_auto()
 					{
 						printf("[CWebTVSetup] skipping: %s\n", webchannel_file);
 					}
-					free(filelist[count]);
 				}
-				free(filelist);
+				free(filelist[count]);
 			}
+			free(filelist);
 		}
 	}
 }
