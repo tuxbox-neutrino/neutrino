8df2710be765da07f5d926221003f42ff400a714
Thilo Graf <dbt@novatux.de>
2020-01-05 21:17:59 +0100

    cc_frm: add global mutex for cc forms

--- src/gui/components/cc_frm.cpp
+++ src/gui/components/cc_frm.cpp
@@ -436,6 +436,8 @@ void CComponentsForm::paintCCItems()
 
 	try{
 		for(size_t i=0; i<v_cc_items.size(); i++){
+			std::lock_guard<std::mutex> g(cc_frm_mutex);
+
 			if (!v_cc_items.at(i))
 				continue;
 
