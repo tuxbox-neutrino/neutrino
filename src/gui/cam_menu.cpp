45b45dd55efbd4c393b310a3eb17896f408f4b20
Thilo Graf <dbt@novatux.de>
2020-01-19 14:26:24 +0100

    cam_menu.cpp: add missing define
    build was broken without #if BOXMODEL_VUPLUS

--- src/gui/cam_menu.cpp
+++ src/gui/cam_menu.cpp
@@ -221,7 +221,9 @@ if (i == 0) { // only for slot 0 valid - fix later
 #else
 			cammenu->addItem(new CMenuOptionNumberChooser(LOCALE_CI_CLOCK, &g_settings.ci_clock[i], true, 6, 12, this));
 #endif
-			cammenu->addItem(new CMenuOptionChooser(LOCALE_CI_RPR, &g_settings.ci_rpr[i], OPTIONS_OFF0_ON1_OPTIONS, OPTIONS_OFF0_ON1_OPTION_COUNT, true, this));
+#if BOXMODEL_VUPLUS
+ 			cammenu->addItem(new CMenuOptionChooser(LOCALE_CI_RPR, &g_settings.ci_rpr[i], OPTIONS_OFF0_ON1_OPTIONS, OPTIONS_OFF0_ON1_OPTION_COUNT, true, this));
+#endif
 }
 		} else {
 			snprintf(str1, sizeof(str1), "%s %d", g_Locale->getText(LOCALE_CI_EMPTY), i);
