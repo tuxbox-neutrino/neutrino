4e89917dbc4c17e163fe7aaba6ccc5872f262451
Jacek Jendrzej <overx300@gmail.com>
2020-01-23 21:43:37 +0100

    netfile: fix invalid conversion

--- src/driver/netfile.cpp
+++ src/driver/netfile.cpp
@@ -200,7 +200,7 @@ static int  request_file(URL *url);
 static int  getCacheSlot(FILE *fd);
 static int  push(FILE *fd, char *buf, long len);
 static int  pop(FILE *fd, char *buf, long len);
-static void CacheFillThread(void *url);
+static void *CacheFillThread(void *url);
 static void ShoutCAST_MetaFilter(STREAM_FILTER *);
 static void ShoutCAST_DestroyFilter(void *a);
 static STREAM_FILTER *ShoutCAST_InitFilter(int);
@@ -545,7 +545,7 @@ int request_file(URL *url)
 		pthread_create(
 			&cache[slot].fill_thread,
 			&cache[slot].attr,
-			(void *(*)(void*))&CacheFillThread, (void*)&cache[slot]);
+			CacheFillThread, (void*)&cache[slot]);
 		dprintf(stderr, "request_file: slot %d fill_thread 0x%x\n", slot, (int)cache[slot].fill_thread);
 	}
 
@@ -1669,12 +1669,12 @@ static bool getChunkSizeLine(STREAM_CACHE *scache,char *line,int size)
 	return false;
 }
 
-void CacheFillThread(void *c)
+void *CacheFillThread(void *c)
 {
 	STREAM_CACHE *scache = (STREAM_CACHE*)c;
 
 	if(scache->closed)
-		return;
+		return NULL;
 
 	set_threadname("netfile:cache");
 	dprintf(stderr, "CacheFillThread: thread started, using stream %p\n", scache->fd);
