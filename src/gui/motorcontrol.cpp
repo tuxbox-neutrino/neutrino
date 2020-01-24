e9211377f963e2a6adf13878f0f9e1939ddc5c13
Jacek Jendrzej <overx300@gmail.com>
2020-01-24 22:38:05 +0100

    motorcontrol: init TP.feparams.pilot

--- src/gui/motorcontrol.cpp
+++ src/gui/motorcontrol.cpp
@@ -138,6 +138,7 @@ int CMotorControl::exec(CMenuTarget* parent, const std::string &)
 	TP.feparams.polarization = scansettings.sat_TP_pol;
 	TP.feparams.delsys = (delivery_system_t) scansettings.sat_TP_delsys;
 	TP.feparams.modulation = (fe_modulation_t) scansettings.sat_TP_mod;
+	TP.feparams.pilot = (zapit_pilot_t) scansettings.sat_TP_pilot;
 	TP.TP_id = 0;
 	g_Zapit->tune_TP(TP);
 
