ef9102b4b1b3f3a1788863b82f3848f2c47640f1
Markus Volk <f_l_k@t-online.de>
2020-03-16 08:28:23 +0100

    videosettings.cpp: set 43Mode options for osmio4k

--- src/gui/videosettings.cpp
+++ src/gui/videosettings.cpp
@@ -721,9 +721,10 @@ bool CVideoSettings::changeNotify(const neutrino_locale_t OptionName, void * /* 
 void CVideoSettings::next43Mode(void)
 {
 	printf("[neutrino VideoSettings] %s setting 43Mode -> ", __FUNCTION__);
-#if HAVE_ARM_HARDWARE
-	/* HD51 has non standard order */
+#if BOXMODEL_HD51 || BOXMODEL_H7 || BOXMODEL_BRE2ZE4K || BOXMODEL_HD60
 	static const char *m[] = { "letterbox", "panscan", "bestfit", "nonlinear", "(unset)" };
+#elif BOXMODEL_OSMIO4K || BOXMODEL_OSMIO4KPLUS
+	static const char *m[] = { "letterbox", "panscan", "scale", "(unset)", "(unset)" };
 #else
 	static const char *m[] = { "panscan", "letterbox", "bestfit", "nonlinear", "(unset)" };
 #endif
