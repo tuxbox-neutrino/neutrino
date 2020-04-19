c9d0f061a81850e3d5c56c9f32183af1914e4e33
svenhoefer <svenhoefer@svenhoefer.com>
2020-04-19 21:35:57 +0200

    - channellist: align offset of minitv to the existing offset of the additional info-box
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/gui/channellist.cpp
+++ src/gui/channellist.cpp
@@ -2459,7 +2459,7 @@ void CChannelList::paintPig(int _x, int _y, int w, int h)
 	if (cc_minitv == NULL){
 		cc_minitv = new CComponentsPIP (0, 0);
 		cc_minitv->setPicture(NEUTRINO_ICON_AUDIOPLAY);
-		cc_minitv->setFrameThickness(OFFSET_INNER_SMALL);
+		cc_minitv->setFrameThickness(OFFSET_INNER_MID);
 	}
 	//set changeable minitv properties
 	cc_minitv->setDimensionsAll(_x, _y, w, h);
