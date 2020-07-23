206bf533a7b77e1ab88de5c5c95df6e5a02509b0
Thilo Graf <dbt@novatux.de>
2020-07-23 18:29:49 +0200

    plugins: add option for software manage integration
    NOTE: bitmask enumeration should be better for more flexibility

--- src/gui/update_menue.cpp
+++ src/gui/update_menue.cpp
@@ -118,13 +118,19 @@ int CSoftwareUpdate::showSoftwareUpdate()
 	softUpdate.addItem(mf);
 #endif
 
+	unsigned int nextShortcut = (unsigned int)softUpdate.getNextShortcut();
+
 #ifdef BOXMODEL_CS_HD2
 	softUpdate.addItem(GenericMenuSeparatorLine);
 
-	mf = new CMenuDForwarder(LOCALE_FLASHUPDATE_CREATEIMAGE_MENU, true, NULL, new CFlashExpertSetup(), NULL, CRCInput::convertDigitToKey(1));
+	mf = new CMenuDForwarder(LOCALE_FLASHUPDATE_CREATEIMAGE_MENU, true, NULL, new CFlashExpertSetup(), NULL, CRCInput::convertDigitToKey(nextShortcut));
 	mf->setHint("", LOCALE_MENU_HINT_SOFTUPDATE_CREATEIMAGE_MENU);
 	softUpdate.addItem(mf);
 #endif
+
+	// plugins with software manage integration
+	nextShortcut = (unsigned int)softUpdate.getNextShortcut();
+	softUpdate.integratePlugins(PLUGIN_INTEGRATION_SOFTWARE_MANAGE, nextShortcut);
 
 	int res = softUpdate.exec (NULL, "");
 	return res;
