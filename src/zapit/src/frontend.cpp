04108628b2e9e07561b8b098384c05b160ed5b9e
svenhoefer <svenhoefer@svenhoefer.com>
2020-10-22 14:24:48 +0200

    - zapit/frontend: re-add cst specific stuff
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/zapit/src/frontend.cpp
+++ src/zapit/src/frontend.cpp
@@ -80,7 +80,11 @@ extern int zapit_debug;
 
 #define FE_COMMON_PROPS	2
 #define FE_DVBS_PROPS	6
+#if HAVE_CST_HARDWARE
+#define FE_DVBS2_PROPS	8
+#else
 #define FE_DVBS2_PROPS	10
+#endif
 #define FE_DVBS2X_PROPS	10
 #define FE_DVBC_PROPS	6
 #define FE_DVBT_PROPS 10
@@ -98,6 +102,20 @@ static const struct dtv_property dvbs_cmdargs[] = {
 	{ DTV_TUNE,		{}     , { 0			}, 0 }
 };
 
+#if HAVE_CST_HARDWARE
+static const struct dtv_property dvbs2_cmdargs[] = {
+	{ DTV_CLEAR,		{0,0,0}, { 0			}, 0 },
+	{ DTV_FREQUENCY,	{}     , { 0			}, 0 },
+	{ DTV_MODULATION,	{}     , { PSK_8		}, 0 },
+	{ DTV_INVERSION,	{}     , { INVERSION_AUTO	}, 0 },
+	{ DTV_SYMBOL_RATE,	{}     , { 27500000		}, 0 },
+	{ DTV_DELIVERY_SYSTEM,	{}     , { SYS_DVBS2		}, 0 },
+	{ DTV_INNER_FEC,	{}     , { FEC_AUTO		}, 0 },
+	{ DTV_PILOT,		{}     , { PILOT_AUTO		}, 0 },
+	{ DTV_ROLLOFF,		{}     , { ROLLOFF_AUTO		}, 0 },
+	{ DTV_TUNE,		{}     , { 0			}, 0 }
+};
+#else
 static const struct dtv_property dvbs2_cmdargs[] = {
 	{ DTV_CLEAR,		{0,0,0}, { 0			}, 0 },
 	{ DTV_FREQUENCY,	{}     , { 0			}, 0 },
@@ -112,6 +130,7 @@ static const struct dtv_property dvbs2_cmdargs[] = {
 	{ DTV_STREAM_ID,	{}     , { NO_STREAM_ID_FILTER	}, 0 },		// twice for BCM45308X
 	{ DTV_TUNE,		{}     , { 0			}, 0 }
 };
+#endif
 
 static const struct dtv_property dvbs2x_cmdargs[] = {
 	{ DTV_CLEAR,		{0,0,0}, { 0			}, 0 },
