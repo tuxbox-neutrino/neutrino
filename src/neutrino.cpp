5132a99b11da265441ba3045d7df13df0fe14866
svenhoefer <svenhoefer@svenhoefer.com>
2020-12-20 23:16:54 +0100

    - enable graphlcd for vu+ only; TODO: needs more fixes
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -691,7 +691,7 @@ int CNeutrinoApp::loadSetup(const char * fname)
 	g_settings.osd_colorsettings_advanced_mode = configfile.getBool("osd_colorsettings_advanced_mode", false);
 
 #ifdef ENABLE_GRAPHLCD
-	g_settings.glcd_enable = configfile.getInt32("glcd_enable", 1);
+	g_settings.glcd_enable = configfile.getInt32("glcd_enable", strcmp(g_info.hw_caps->boxvendor, "VU+") == 0);
 
 	g_settings.glcd_time_in_standby = configfile.getInt32("glcd_time_in_standby", 1);
 	g_settings.glcd_standby_weather = configfile.getInt32("glcd_standby_weather", 1);
@@ -1356,10 +1356,17 @@ void CNeutrinoApp::upgradeSetup(const char * fname)
 			g_settings.timer_followscreenings = 2 /*always*/;
 		configfile.deleteKey("recording_tevents");
 	}
-	if (g_settings.version_pseudo < "20170711000000")
+	if (g_settings.version_pseudo < "20200711000000")
 	{
 #ifdef ENABLE_GRAPHLCD
 		configfile.deleteKey("glcd_show_logo");
+#endif
+	}
+	if (g_settings.version_pseudo < "20201220000000")
+	{
+#ifdef ENABLE_GRAPHLCD
+		if (strcmp(g_info.hw_caps->boxvendor, "VU+") != 0)
+			g_settings.glcd_enable = 0;
 #endif
 	}
 
