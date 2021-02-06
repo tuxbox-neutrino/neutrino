64d60988cd90c5be88491af1799becf4e86eee0c
OneOfNine <oneofnine@web.de>
2021-02-06 13:26:26 +0100

    icons: update default iconset
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/gui/widget/icons.h
+++ src/gui/widget/icons.h
@@ -90,6 +90,7 @@
 #define NEUTRINO_ICON_BOOKMARK_MANAGER	"bookmarkmanager"
 #define NEUTRINO_ICON_COLORS		"colors"
 #define NEUTRINO_ICON_EDIT		"edit"
+#define NEUTRINO_ICON_EDIT_FIND		"edit_find"
 #define NEUTRINO_ICON_ERROR		"error"
 #define NEUTRINO_ICON_FEATURES		"features"
 #define NEUTRINO_ICON_GAMES		"games"
