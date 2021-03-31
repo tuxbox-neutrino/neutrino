fe71518e5043f4d323a90ade1a209b5e8fadf35f
Frankenstone <dampf_acc@online.de>
2021-03-31 22:15:28 +0200

    osd_setup: osd preset crt/lcd add locals use now for different Screen Setup when using different monitors
     Conflicts:
    	data/locale/deutsch.locale
    	data/locale/english.locale
    	src/system/locals.h
    	src/system/locals_intern.h

--- src/gui/osd_setup.h
+++ src/gui/osd_setup.h
@@ -101,8 +101,8 @@ class COsdSetup : public CMenuTarget, public CChangeObserver
 
 		enum
 		{
-			PRESET_CRT,
-			PRESET_LCD
+			PRESET_LCD,
+			PRESET_CRT
 		};
 		
 		COsdSetup(int wizard_mode = SNeutrinoSettings::WIZARD_OFF);
