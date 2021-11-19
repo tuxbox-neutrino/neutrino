e52eb3d144c41026a33fdbfaf88fe18294bbde31
svenhoefer <svenhoefer@svenhoefer.com>
2021-11-19 23:05:40 +0100

    - luainstance: formatting code using astyle
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/gui/lua/luainstance.h
+++ src/gui/lua/luainstance.h
@@ -30,53 +30,54 @@ extern "C" {
 
 #include "luainstance_helpers.h"
 
-void LuaInstRegisterFunctions(lua_State *L, bool fromThreads=false);
+void LuaInstRegisterFunctions(lua_State *L, bool fromThreads = false);
 
 /* inspired by Steve Kemp http://www.steve.org.uk/ */
 class CLuaInstance
 {
-	static const char className[];
-	static CLuaData *CheckData(lua_State *L, int narg);
-public:
-	CLuaInstance();
-	~CLuaInstance();
-	void runScript(const char *fileName, std::vector<std::string> *argv = NULL, std::string *result_code = NULL, std::string *result_string = NULL, std::string *error_string = NULL);
-	void abortScript();
-	bool DisableAbort;
-	enum {
-		DYNFONT_NO_ERROR      = 0,
-		DYNFONT_MAXIMUM_FONTS = 1,
-		DYNFONT_TOO_WIDE      = 2,
-		DYNFONT_TOO_HIGH      = 3
-	};
+		static const char className[];
+		static CLuaData *CheckData(lua_State *L, int narg);
+	public:
+		CLuaInstance();
+		~CLuaInstance();
+		void runScript(const char *fileName, std::vector<std::string> *argv = NULL, std::string *result_code = NULL, std::string *result_string = NULL, std::string *error_string = NULL);
+		void abortScript();
+		bool DisableAbort;
+		enum
+		{
+			DYNFONT_NO_ERROR      = 0,
+			DYNFONT_MAXIMUM_FONTS = 1,
+			DYNFONT_TOO_WIDE      = 2,
+			DYNFONT_TOO_HIGH      = 3
+		};
 
-	// Example: runScript(fileName, "Arg1", "Arg2", "Arg3", ..., NULL);
-	//	Type of all parameters: const char*
-	//	The last parameter to NULL is imperative.
-	void runScript(const char *fileName, const char *arg0, ...);
+		// Example: runScript(fileName, "Arg1", "Arg2", "Arg3", ..., NULL);
+		//	Type of all parameters: const char*
+		//	The last parameter to NULL is imperative.
+		void runScript(const char *fileName, const char *arg0, ...);
 
-	static int NewWindow(lua_State *L);
-	static int GCWindow(lua_State *L);
-	static int GetInput(lua_State *L);
-	static int Blit(lua_State *L);
-	static int GetLanguage(lua_State *L);
-	static int PaintBox(lua_State *L);
-	static int paintHLineRel(lua_State *L);
-	static int paintVLineRel(lua_State *L);
-	static int RenderString(lua_State *L);
-	static int getRenderWidth(lua_State *L);
-	static int FontHeight(lua_State *L);
-	static int getDynFont(lua_State *L);
-	static int PaintIcon(lua_State *L);
-	static int DisplayImage(lua_State *L);
-	static int GetSize(lua_State *L);
-	static int saveScreen(lua_State *L);
-	static int restoreScreen(lua_State *L);
-	static int deleteSavedScreen(lua_State *L);
-	static int scale2Res(lua_State *L);
+		static int NewWindow(lua_State *L);
+		static int GCWindow(lua_State *L);
+		static int GetInput(lua_State *L);
+		static int Blit(lua_State *L);
+		static int GetLanguage(lua_State *L);
+		static int PaintBox(lua_State *L);
+		static int paintHLineRel(lua_State *L);
+		static int paintVLineRel(lua_State *L);
+		static int RenderString(lua_State *L);
+		static int getRenderWidth(lua_State *L);
+		static int FontHeight(lua_State *L);
+		static int getDynFont(lua_State *L);
+		static int PaintIcon(lua_State *L);
+		static int DisplayImage(lua_State *L);
+		static int GetSize(lua_State *L);
+		static int saveScreen(lua_State *L);
+		static int restoreScreen(lua_State *L);
+		static int deleteSavedScreen(lua_State *L);
+		static int scale2Res(lua_State *L);
 
-private:
-	lua_State* lua;
+	private:
+		lua_State *lua;
 
 };
 
