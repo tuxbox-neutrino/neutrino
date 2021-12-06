ebf57c0b2ede11f1c4dff3919aa82c69d10dddc7
svenhoefer <svenhoefer@svenhoefer.com>
2021-12-06 05:18:19 +0100

    - start-wizard: remove Astrasat code
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/gui/start_wizard.cpp
+++ src/gui/start_wizard.cpp
@@ -69,14 +69,6 @@ CStartUpWizard::~CStartUpWizard()
 {
 }
 
-#ifdef ENABLE_FASTSCAN
-const CMenuOptionChooser::keyval WIZARD_SETUP_TYPE[] =
-{
-	{ 0, LOCALE_WIZARD_SETUP_EASY },
-	{ 1, LOCALE_WIZARD_SETUP_ADVANCED },
-};
-#endif
-
 int CStartUpWizard::exec(CMenuTarget *parent, const string &/*actionKey*/)
 {
 	// remove menu-timeout during wizard
@@ -107,34 +99,21 @@ int CStartUpWizard::exec(CMenuTarget *parent, const string &/*actionKey*/)
 
 	//if (ShowMsg(LOCALE_WIZARD_WELCOME_HEAD, g_Locale->getText(LOCALE_WIZARD_WELCOME_TEXT), CMsgBox::mbrYes, CMsgBox::mbYes | CMsgBox::mbrCancel) == CMsgBox::mbrYes)
 	{
-		int advanced = 1;
-#ifdef ENABLE_FASTSCAN
-		std::string lang = g_settings.language;
-		if (lang == "nederlands")
-		{
-			advanced = 0;
-			CMenuWidget wtype(LOCALE_WIZARD_SETUP);
-			wtype.setWizardMode(SNeutrinoSettings::WIZARD_ON);
-			wtype.addIntroItems();
-			CMenuOptionChooser *mc = new CMenuOptionChooser(LOCALE_WIZARD_SETUP_TYPE, &advanced, WIZARD_SETUP_TYPE, 2, true, NULL);
-			mc->setHint("", LOCALE_WIZARD_SETUP_TYPE_HINT);
-			wtype.addItem(mc);
-			wtype.exec(NULL, "");
-		}
-#endif
 		// open video settings in wizardmode
-		if (advanced && res != menu_return::RETURN_EXIT_ALL)
+		if (res != menu_return::RETURN_EXIT_ALL)
 		{
 			g_videoSettings->setWizardMode(SNeutrinoSettings::WIZARD_ON);
 			res = g_videoSettings->exec(NULL, "");
 			g_videoSettings->setWizardMode(SNeutrinoSettings::WIZARD_OFF);
 		}
-		if (advanced && res != menu_return::RETURN_EXIT_ALL)
+
+		if (res != menu_return::RETURN_EXIT_ALL)
 		{
 			COsdSetup osdSettings(SNeutrinoSettings::WIZARD_ON);
 			res = osdSettings.exec(NULL, "");
 		}
-		if (advanced && res != menu_return::RETURN_EXIT_ALL)
+
+		if (res != menu_return::RETURN_EXIT_ALL)
 		{
 			CNetworkSetup::getInstance()->setWizardMode(SNeutrinoSettings::WIZARD_ON);
 			res = CNetworkSetup::getInstance()->exec(NULL, "");
@@ -143,7 +122,7 @@ int CStartUpWizard::exec(CMenuTarget *parent, const string &/*actionKey*/)
 
 #if ENABLE_PKG_MANAGEMENT
 		// package update check
-		if (advanced && res != menu_return::RETURN_EXIT_ALL)
+		if (res != menu_return::RETURN_EXIT_ALL)
 		{
 			COPKGManager man(SNeutrinoSettings::WIZARD_START);
 			if (man.hasOpkgSupport())
@@ -162,7 +141,7 @@ int CStartUpWizard::exec(CMenuTarget *parent, const string &/*actionKey*/)
 		if (CFEManager::getInstance()->haveSat())
 			init_settings = file_exists(CONFIGDIR "/initial/");
 
-		if (advanced && init_settings && (res != menu_return::RETURN_EXIT_ALL))
+		if (init_settings && res != menu_return::RETURN_EXIT_ALL)
 		{
 			if (ShowMsg(LOCALE_WIZARD_INITIAL_SETTINGS, g_Locale->getText(LOCALE_WIZARD_INSTALL_SETTINGS),
 					CMsgBox::mbrYes, CMsgBox::mbYes | CMsgBox::mbNo, NULL, 450, 30) == CMsgBox::mbrYes)
@@ -176,32 +155,9 @@ int CStartUpWizard::exec(CMenuTarget *parent, const string &/*actionKey*/)
 		if (res != menu_return::RETURN_EXIT_ALL)
 		{
 			CScanSetup::getInstance()->setWizardMode(SNeutrinoSettings::WIZARD_ON);
-			if (advanced)
-			{
-				res = CScanSetup::getInstance()->exec(NULL, "setup_frontend");
-				if (res != menu_return::RETURN_EXIT_ALL)
-					res = CScanSetup::getInstance()->exec(NULL, "");
-			}
-			else
-			{
-				CZapit::getInstance()->GetConfig(zapitCfg);
-#ifdef ENABLE_FASTSCAN
-				if (CFEManager::getInstance()->haveSat())
-				{
-					CMenuWidget fastScanMenu(LOCALE_SATSETUP_FASTSCAN_HEAD, NEUTRINO_ICON_SETTINGS, 45, MN_WIDGET_ID_SCAN_FAST_SCAN);
-					fastScanMenu.setWizardMode(SNeutrinoSettings::WIZARD_ON);
-					CScanSetup::getInstance()->addScanMenuFastScan(&fastScanMenu);
-					res = fastScanMenu.exec(NULL, "");
-				}
-#endif
-				if (CFEManager::getInstance()->haveCable())
-				{
-					CMenuWidget cableScan(LOCALE_SATSETUP_CABLE, NEUTRINO_ICON_SETTINGS, 45, MN_WIDGET_ID_SCAN_CABLE_SCAN);
-					cableScan.setWizardMode(SNeutrinoSettings::WIZARD_ON);
-					CScanSetup::getInstance()->addScanMenuCable(&cableScan);
-					res = cableScan.exec(NULL, "");
-				}
-			}
+			res = CScanSetup::getInstance()->exec(NULL, "setup_frontend");
+			if (res != menu_return::RETURN_EXIT_ALL)
+				res = CScanSetup::getInstance()->exec(NULL, "");
 			CScanSetup::getInstance()->setWizardMode(SNeutrinoSettings::WIZARD_OFF);
 		}
 	}
