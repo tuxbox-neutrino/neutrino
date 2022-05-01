8106d9ad54f5e3e79c8c592521d34f8bbd1271d4
svenhoefer <svenhoefer@svenhoefer.com>
2022-05-01 07:01:22 +0200

    - moviebrowser: fix segfault
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/gui/moviebrowser/mb.cpp
+++ src/gui/moviebrowser/mb.cpp
@@ -3612,7 +3612,7 @@ void CMovieBrowser::initOptionsBrowserMenu(CMenuWidget *OptionsMenuBrowser, CInt
 
 bool CMovieBrowser::showMenu(bool calledExternally)
 {
-	if (m_header->getClockObject())
+	if (m_header && m_header->getClockObject())
 		m_header->disableClock();
 
 	/* first clear screen */
@@ -3754,7 +3754,7 @@ bool CMovieBrowser::showMenu(bool calledExternally)
 
 	delete nfs;
 
-	if (m_header->getClockObject())
+	if (m_header && m_header->getClockObject())
 		m_header->enableClock();
 
 	return(true);
