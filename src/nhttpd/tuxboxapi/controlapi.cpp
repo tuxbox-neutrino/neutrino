afe23e002b84e27170c12d5f179c19bd49cf2922
svenhoefer <svenhoefer@svenhoefer.com>
2022-09-14 20:47:52 +0200

    - controlapi: add mode_moviebrowser to GetModeCGI()
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/nhttpd/tuxboxapi/controlapi.cpp
+++ src/nhttpd/tuxboxapi/controlapi.cpp
@@ -488,6 +488,8 @@ void CControlAPI::GetModeCGI(CyhookHandler *hh)
 			result = "upnp";
 		else if (mode == NeutrinoModes::mode_webradio)
 			result = "webradio";
+		else if (mode == NeutrinoModes::mode_moviebrowser)
+			result = "moviebrowser";
 		else
 			result = "unknown";
 	}
