edea36b9b418a94756de0793dc159894ef3b0c6f
svenhoefer <svenhoefer@svenhoefer.com>
2022-09-16 23:29:29 +0200

    - controlapi: fix and simplify RC_DEVICE defines
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/nhttpd/tuxboxapi/controlapi.cpp
+++ src/nhttpd/tuxboxapi/controlapi.cpp
@@ -60,13 +60,38 @@ extern CPlugins *g_Plugins;//for relodplugins
 extern CBouquetManager *g_bouquetManager;
 
 #if HAVE_CST_HARDWARE
-	#define RC_DEVICE "/dev/input/nevis_ir"
-#elif BOXMODEL_H7
-	#define RC_DEVICE "/dev/input/event2"
-#else
-	#define RC_DEVICE "/dev/input/event1"
+#ifndef RC_DEVICE
+#define RC_DEVICE "/dev/input/nevis_ir"
 #endif
+#ifndef RC_DEVICE_FALLBACK
 #define RC_DEVICE_FALLBACK "/dev/input/event0"
+#endif
+
+#elif BOXMODEL_H7
+#ifndef RC_DEVICE
+#define RC_DEVICE "/dev/input/event2"
+#endif
+#ifndef RC_DEVICE_FALLBACK
+#define RC_DEVICE_FALLBACK "/dev/input/event1"
+#endif
+
+#elif BOXMODEL_MULTIBOX || BOXMODEL_MULTIBOXSE || BOXMODEL_OSMIO4K || BOXMODEL_OSMIO4KPLUS
+#ifndef RC_DEVICE
+#define RC_DEVICE "/dev/input/event0"
+#endif
+#ifndef RC_DEVICE_FALLBACK
+#define RC_DEVICE_FALLBACK "/dev/input/event1"
+#endif
+
+#else
+#ifndef RC_DEVICE
+#define RC_DEVICE "/dev/input/event1"
+#endif
+#ifndef RC_DEVICE_FALLBACK
+#define RC_DEVICE_FALLBACK "/dev/input/event0"
+#endif
+
+#endif
 
 //-----------------------------------------------------------------------------
 //=============================================================================
