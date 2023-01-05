88ea3e6b30dcb8d3c7938af129a411bb18147830
Thilo Graf <dbt@novatux.de>
2023-01-05 23:13:39 +0100

    moviebrowser: remove rezap on leaving of moviebrowser
    Comes in with - moviebrowser: add lcd4linux support
     260ca7d64ec0ff0b65753e12c1a97db0c967161c.
    
    Why is rezap required here?

--- src/gui/moviebrowser/mb.cpp
+++ src/gui/moviebrowser/mb.cpp
@@ -1188,7 +1188,7 @@ int CMovieBrowser::exec(const char* path)
 		fileInfoStale();
 	}
 
-	CNeutrinoApp::getInstance()->handleMsg(NeutrinoMessages::CHANGEMODE, m_LastMode);
+	CNeutrinoApp::getInstance()->handleMsg(NeutrinoMessages::CHANGEMODE, m_LastMode | NeutrinoModes::norezap);
 
 	return (res);
 }
