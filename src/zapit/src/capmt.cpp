93dd22886d9bda81afbe7ab43afeda8b1dda49f0
BPanther <bpanther_ts@hotmail.com>
2023-10-12 20:54:17 +0200

    fix compiler warning
    Origin commit data
    ------------------
    Commit: https://github.com/Duckbox-Developers/neutrino-ddt/commit/e7e408f8766894b66e6b174e79658a7415c6ee6d
    Author: BPanther <bpanther_ts@hotmail.com>
    Date: 2023-10-12 (Thu, 12 Oct 2023)

--- src/zapit/src/capmt.cpp
+++ src/zapit/src/capmt.cpp
@@ -290,7 +290,6 @@ bool CCamManager::SetMode(t_channel_id channel_id, enum runmode mode, bool start
 #endif
 			INFO("RECORD(%d): fe_num %d rec_dmx %d", mode, source, demux);
 			break;
-#ifdef ENABLE_PIP
 		case PIP:
 #if HAVE_ARM_HARDWARE || HAVE_MIPS_HARDWARE
 #ifdef DYNAMIC_DEMUX
@@ -312,7 +311,6 @@ bool CCamManager::SetMode(t_channel_id channel_id, enum runmode mode, bool start
 #endif
 			INFO("PIP: fe_num %d dmx_src %d", source, demux);
 			break;
-#endif
 	}
 
 	oldmask = cam->getCaMask();
