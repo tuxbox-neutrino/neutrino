501a5e87339967f398c1b3a56bb704ff7d002eeb
TangoCash <eric@loxat.de>
2024-10-07 20:24:49 +0200

    streamts: add fallback to FindChannel48()
    Origin commit data
    ------------------
    Commit: https://github.com/neutrino-images/ni-neutrino/commit/4ebfb3d8ff94e680c43a7c85e0abf85c20880218
    Author: TangoCash <eric@loxat.de>
    Date: 2024-10-07 (Mon, 07 Oct 2024)
    
    Origin message was:
    ------------------
    - streamts: add fallback to FindChannel48()
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- src/driver/streamts.cpp
+++ src/driver/streamts.cpp
@@ -474,6 +474,8 @@ bool CStreamManager::Parse(int fd, stream_pids_t &pids, t_channel_id &chid, CFro
 		{
 			tmpid = (((t_channel_id)i3) << 32) | (((t_channel_id)i4) << 16) | (t_channel_id)i2;
 			tmp_channel = CServiceManager::getInstance()->FindChannel48Pos(tmpid, satpos >> 16);
+			if (tmp_channel == NULL)
+				tmp_channel = CServiceManager::getInstance()->FindChannel48(tmpid);
 			if (tmp_channel)
 			{
 				printf("CStreamManager::Parse:E: channel_id %" PRIx64 " [%s] \n", tmp_channel->getChannelID(), tmp_channel->getName().c_str());
