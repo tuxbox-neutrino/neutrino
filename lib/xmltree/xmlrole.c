fa5c50bbe9caef732c3dadece04019817d2ba447
Thilo Graf <dbt@novatux.de>
2026-04-09 08:14:35 +0200

    lib/xmltree/xmlrole: try to fix possible fall through
    warning was: -Wimplicit-fallthrough=

--- lib/xmltree/xmlrole.c
+++ lib/xmltree/xmlrole.c
@@ -76,18 +76,19 @@ int prolog0(PROLOG_STATE *state,
     return XML_ROLE_NONE;
   case XML_TOK_COMMENT:
     state->handler = prolog1;
-  case XML_TOK_BOM:
     return XML_ROLE_NONE;
   case XML_TOK_DECL_OPEN:
-    if (!XmlNameMatchesAscii(enc,
-			     ptr + 2 * MIN_BYTES_PER_CHAR(enc),
-			     "DOCTYPE"))
+    if (!XmlNameMatchesAscii(enc, ptr + 2 * MIN_BYTES_PER_CHAR(enc), "DOCTYPE"))
       break;
-    state->handler = doctype0;
-    return XML_ROLE_NONE;
+    else{
+      state->handler = doctype0;
+      return XML_ROLE_NONE;
+    }
   case XML_TOK_INSTANCE_START:
     state->handler = error;
     return XML_ROLE_INSTANCE_START;
+  case XML_TOK_BOM:
+    return XML_ROLE_NONE;
   }
   return syntaxError(state);
 }
